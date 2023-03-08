c179468997dbf2b0dcc535f296a0fab226f47b80
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-03-07 23:59:22 -0500

    feat(operators): Add impl

--- operators/Add.ts
+++ operators/Add.ts
@@ -1,9 +1,14 @@
 import { State, ValueType } from '../types'
 import { Operator } from './Operator'
 
-class Add extends Operator {
+export class Add extends Operator {
   evaluate (state: State): void {
     this.checkStack(state, ValueType.number, ValueType.number)
-    const ops = state.stack.slice(0, 2)
+    const [first, second] = state.stack.slice(0, 2).map(value => value.data as number)
+    state.stack.splice(0, 2)
+    state.stack.unshift({
+      type: ValueType.number,
+      data: first + second
+    })
   }
 }
