43d997d91222b06edf42cfbb2b33aa43998db690
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-08 19:07:55 +0000

    test(state): state creation

--- test-helpers.ts
+++ test-helpers.ts
@@ -1,8 +1,29 @@
-import { State } from './types'
+import { IState, Value, ValueType } from './types'
+import { State } from './state'
+import { StackUnderflow } from './errors'
 
-export function createState (): State {
-  return {
-    contexts: [],
-    stack: []
-  }
+type StackItem = string | number | Value
+interface StackInitializer {
+  stack?: StackItem[]
+}
+
+export function createState ({
+  stack
+}: StackInitializer = {}): IState {
+  const initialStack = (stack ?? []).map((item: StackItem): Value => {
+    if (typeof item === 'string') {
+      return {
+        type: ValueType.string,
+        data: item
+      }
+    }
+    if (typeof item === 'number') {
+      return {
+        type: ValueType.number,
+        data: item
+      }
+    }
+    return item
+  })
+  return new State(initialStack)
 }
