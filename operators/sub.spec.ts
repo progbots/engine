22ff0c5ef7888baba5893c346b86cbf4d4c7483c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-08 19:08:41 +0000

    feat(sub): implement

---
+++ operators/sub.spec.ts
@@ -0,0 +1,56 @@
+import { ValueType } from '../types'
+import { StackUnderflow, TypeCheck } from '../errors'
+import { sub } from './sub'
+import { createState } from '../test-helpers'
+
+describe('operators/sub', () => {
+  it('substracts two numbers', () => {
+    const state = createState({
+      stack: [3, 2]
+    })
+    sub(state)
+    expect(state.index(0)).toStrictEqual({
+      type: ValueType.number,
+      data: 1
+    })
+  })
+
+  describe('errors', () => {
+    describe('StackUnderflow: it requires 2 stack items', () => {
+      it('fails on empty stack', () => {
+        const state = createState()
+        expect(() => sub(state)).toThrowError(StackUnderflow)
+      })
+
+      it('fails with only one stack item', () => {
+        const state = createState({
+          stack: [1]
+        })
+        expect(() => sub(state)).toThrowError(StackUnderflow)
+      })
+    })
+
+    describe('TypeCheck: it requires only numbers', () => {
+      it('fails on strings', () => {
+        const state = createState({
+          stack: ['a', 'b']
+        })
+        expect(() => sub(state)).toThrowError(TypeCheck)
+      })
+
+      it('fails on mixed parameters (number, string)', () => {
+        const state = createState({
+          stack: [1, 'b']
+        })
+        expect(() => sub(state)).toThrowError(TypeCheck)
+      })
+
+      it('fails on mixed parameters (string, number)', () => {
+        const state = createState({
+          stack: ['a', 2]
+        })
+        expect(() => sub(state)).toThrowError(TypeCheck)
+      })
+    })
+  })
+})
