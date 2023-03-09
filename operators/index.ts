3d49f3a11504afec408f9c3b579b16dbf9fe308c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-09 16:02:38 +0000

    chores(operators):  index all operators

--- operators/index.ts
+++ operators/index.ts
@@ -1,9 +1,3 @@
-import { checkStack } from '../state'
-import { IState, ValueType } from '../types'
-
-export function index (state: IState): void {
-  const [pos] = checkStack(state, ValueType.number).map(value => value.data as number)
-  state.pop()
-  const value = state.index(pos)
-  state.push(value)
-}
+export * from './add'
+export * from './index-op'
+export * from './sub'
