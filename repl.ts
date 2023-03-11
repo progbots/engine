418b9cffd5b5a3c3f292a405c32a29483650a3c6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-03-11 00:12:37 -0500

    feat(repl): handle errors

--- repl.ts
+++ repl.ts
@@ -2,19 +2,37 @@ import * as readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
 import { State } from './state'
 import { parse } from './parser'
+import { BaseError } from './errors/BaseError'
 
 async function main (): Promise<void> {
   const rl = readline.createInterface({ input, output })
-  console.log('Use exit to quit')
+  console.log('Use .exit to quit')
+  console.log('Use .pstack to show state')
   const state = new State()
 
   while (true) {
-    const src = await rl.question('> ')
-    if (src === 'exit') {
+    const src = await rl.question(`${state.stack().length}> `)
+    if (src === '.exit') {
       break
     }
-    const block = parse(src, state)
-    console.log(block)
+    if (src === '.pstack') {
+      console.log(`Contexts count : ${state.contexts().length}`)
+      const reversedStack = [...state.stack()].reverse()
+      reversedStack.forEach((value, index) => {
+        console.log(`[${reversedStack.length - index - 1}] ${value.type} ${value.data.toString()}`)
+      })
+    } else {
+      try {
+        parse(src, state).forEach(value => state.eval(value))
+      } catch (e) {
+        if (e instanceof BaseError) {
+          console.log(`-${e.name}- ${e.message}`)
+        } else {
+          console.error(e)
+          break
+        }
+      }
+    }
   }
 
   rl.close()
