535e314cde22debe7f409b40b9b31ebaf4ef90f0
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-13 19:03:21 +0000

    feat(check-state): check non typed items with null

--- operators/check-state.ts
+++ operators/check-state.ts
@@ -1,14 +1,14 @@
 import { IState, Value, ValueType } from '../types'
 import { StackUnderflow, TypeCheck } from '../errors'
 
-export function checkStack (state: IState, ...types: ValueType[]): Value[] {
+export function checkStack (state: IState, ...types: (ValueType | null)[]): Value[] {
   const stack = state.stack()
   if (types.length > stack.length) {
     throw new StackUnderflow()
   }
-  return types.map((type: ValueType, pos: number): Value => {
+  return types.map((type: ValueType | null, pos: number): Value => {
     const value = stack[pos]
-    if (value.type !== type) {
+    if (type !== null && value.type !== type) {
       throw new TypeCheck()
     }
     return value
