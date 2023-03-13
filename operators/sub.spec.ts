1d175c8df6048a0035eb949fbbaec210a2c5c756
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-13 18:48:54 +0000

    refactor(tests): implements a generic test runner

--- operators/sub.spec.ts
+++ operators/sub.spec.ts
@@ -1,56 +1,30 @@
-import { ValueType } from '../types'
 import { StackUnderflow, TypeCheck } from '../errors'
-import { sub } from './sub'
-import { createState } from '../test-helpers'
+import { executeTests } from '../test-helpers'
 
 describe('operators/sub', () => {
-  it('substracts two numbers', () => {
-    const state = createState({
-      stack: [3, 2]
-    })
-    sub(state)
-    expect(state.stack()[0]).toStrictEqual({
-      type: ValueType.integer,
-      data: 1
-    })
-  })
-
-  describe('errors', () => {
-    describe('StackUnderflow: it requires 2 stack items', () => {
-      it('fails on empty stack', () => {
-        const state = createState()
-        expect(() => sub(state)).toThrowError(StackUnderflow)
-      })
-
-      it('fails with only one stack item', () => {
-        const state = createState({
-          stack: [1]
-        })
-        expect(() => sub(state)).toThrowError(StackUnderflow)
-      })
-    })
-
-    describe('TypeCheck: it requires only numbers', () => {
-      it('fails on strings', () => {
-        const state = createState({
-          stack: ['a', 'b']
-        })
-        expect(() => sub(state)).toThrowError(TypeCheck)
-      })
-
-      it('fails on mixed parameters (number, string)', () => {
-        const state = createState({
-          stack: [1, 'b']
-        })
-        expect(() => sub(state)).toThrowError(TypeCheck)
-      })
-
-      it('fails on mixed parameters (string, number)', () => {
-        const state = createState({
-          stack: ['a', 2]
-        })
-        expect(() => sub(state)).toThrowError(TypeCheck)
-      })
-    })
+  executeTests({
+    'substracts two numbers': {
+      src: '3 2 sub',
+      expect: '-1'
+    },
+    'fails with StackUnderflow on empty stack': {
+      src: 'sub',
+      error: StackUnderflow
+    },
+    'fails with StackUnderflow on a single stack item': {
+      src: '1 sub',
+      error: StackUnderflow
+    },
+    'fails with TypeCheck when no numbers are used': {
+      src: '"a" "b" sub',
+      error: TypeCheck
+    },
+    'fails with TypeCheck when mixed parameters are used': [{
+      src: '"a" 2 sub',
+      error: TypeCheck
+    }, {
+      src: '1 "b" sub',
+      error: TypeCheck
+    }]
   })
 })
