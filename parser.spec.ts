b749abe0bdb429f8ea386469612880fc0ffb6d1c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-13 18:36:59 +0000

    feat(parser): add strings and negative integers

--- parser.spec.ts
+++ parser.spec.ts
@@ -1,18 +1,37 @@
 import { State } from './state'
-import { ValueType } from './types'
+import { Value, ValueType } from './types'
 import { parse } from './parser'
 
 describe('parser', () => {
-  it('generates a value', () => {
-    const state = new State()
-    const value = [...parse('1', state)]
-    expect(value).toStrictEqual([{
+  const values: Record<string, Value> = {
+    1: {
       type: ValueType.integer,
       data: 1
-    }])
+    },
+    '-1': {
+      type: ValueType.integer,
+      data: -1
+    },
+    add: {
+      type: ValueType.name,
+      data: 'add'
+    },
+    '"a"': {
+      type: ValueType.string,
+      data: 'a'
+    }
+  }
+
+  Object.keys(values).forEach(src => {
+    const expected = values[src]
+    it(`generates ${expected.type} ${JSON.stringify(expected.data)}`, () => {
+      const state = new State()
+      const value = [...parse(src, state)]
+      expect(value).toStrictEqual([expected])
+    })
   })
 
-  it('generates a block of values', () => {
+  it('generates values', () => {
     const state = new State()
     const block = [...parse('1 2 add', state)]
     expect(block).toStrictEqual([{
--- parser.ts
+++ parser.ts
@@ -1,19 +1,24 @@
 import { IState, Value, ValueType } from './types'
 
 export function * parse (src: string, state: IState): Generator<Value> {
-  const matcher = /(\d+)|(\w+)/g
+  const matcher = /(?:"([^"]*)")|(-?\d+)|(\w+)/g
   let match = matcher.exec(src)
   while (match !== null) {
-    const [, digits, name] = match
-    if (digits !== undefined) {
+    const [, string, integer, name] = match
+    if (integer !== undefined) {
       yield {
         type: ValueType.integer,
-        data: parseInt(digits, 10)
+        data: parseInt(integer, 10)
       }
     } else if (name !== undefined) {
       yield {
         type: ValueType.name,
         data: name
+      }
+    } else if (string !== undefined) {
+      yield {
+        type: ValueType.string,
+        data: string
       }
     }
     match = matcher.exec(src)
