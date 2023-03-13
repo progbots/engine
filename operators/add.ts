191abf6f8d1edc9d5ce783802c39d2dd7f8d3c34
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-13 18:49:08 +0000

    feat(check-state): isolates check-state helper

--- operators/add.ts
+++ operators/add.ts
@@ -1,12 +1,12 @@
 import { IState, ValueType } from '../types'
-import { checkStack } from '../state'
+import { checkStack } from './check-state'
 
 export function add (state: IState): void {
-  const [first, second] = checkStack(state, ValueType.integer, ValueType.integer).map(value => value.data as number)
+  const [num1, num2] = checkStack(state, ValueType.integer, ValueType.integer).map(value => value.data as number)
   state.pop()
   state.pop()
   state.push({
     type: ValueType.integer,
-    data: first + second
+    data: num1 + num2
   })
 }
