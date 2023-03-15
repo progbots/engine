15e0e0bf7604b7eac3f6d4c442a1e0898d2a44fe
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-15 14:51:06 +0000

    refactor(state): cycles is no more exposed

--- state/cycle.ts
+++
@@ -1,9 +0,0 @@
-export function cycles (iterator: Generator<void>): number {
-  let count = 0
-  let { done } = iterator.next()
-  while (done === false) {
-    ++count
-    done = iterator.next().done
-  }
-  return count
-}
