75402457a89ce479e2c8ab6d7a775becce83fb15
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-03-19 21:35:12 -0400

    refactor(objects/BaseArray): isolate memory tracking methods

--- objects/BaseArray.ts
+++ objects/BaseArray.ts
@@ -35,12 +35,21 @@ export abstract class BaseArray extends ShareableObject implements IArray {
 
   // endregion IArray
 
+  protected addValueRef (value: Value): void {
+    this._memoryTracker.addValueRef(value)
+    this._memoryTracker.increment(BaseArray.VALUE_ADDITIONAL_SIZE)
+  }
+
   protected abstract pushImpl (value: Value): void
 
   push (value: Value): void {
-    this._memoryTracker.addValueRef(value)
-    this._memoryTracker.increment(BaseArray.VALUE_ADDITIONAL_SIZE)
+    this.addValueRef(value)
     this.pushImpl(value)
+  }
+
+  protected releaseValue (value: Value): void {
+    this._memoryTracker.releaseValue(value)
+    this._memoryTracker.decrement(BaseArray.VALUE_ADDITIONAL_SIZE)
   }
 
   protected abstract popImpl (): Value
@@ -50,8 +59,7 @@ export abstract class BaseArray extends ShareableObject implements IArray {
       throw new StackUnderflow()
     }
     const value = this.popImpl()
-    this._memoryTracker.releaseValue(value)
-    this._memoryTracker.decrement(BaseArray.VALUE_ADDITIONAL_SIZE)
+    this.releaseValue(value)
   }
 
   get ref (): readonly Value[] {
