794537b8fd3bbd63caa3b8154f5a6c079262d6c0
Arnaud Buchholz <arnaud.buchholz@free.fr>
2023-03-28 01:07:39 +0000

    feat(operators/set): set

---
+++ operators/set.spec.ts
@@ -0,0 +1,52 @@
+import { RangeCheck, StackUnderflow, TypeCheck } from '../errors'
+import { executeTests } from '../test-helpers'
+
+describe('operators/set', () => {
+  executeTests({
+    'sets item in an array': {
+      src: '[31 41 59] 1 42 set',
+      expect: '[31 42 59]'
+    },
+    'sets character code of a string': {
+      src: '"abc" 1 66 set',
+      expect: '"aBc"'
+    },
+    'sets item in a dictionary': {
+      src: 'dict /test 42 set /test get',
+      expect: '42'
+    },
+    'fails with StackUnderflow on empty stack': {
+      src: 'set',
+      error: StackUnderflow
+    },
+    'fails with StackUnderflow if parameters are missing': [{
+      src: '0 set',
+      error: StackUnderflow
+    }, {
+      src: '0 0 set',
+      error: StackUnderflow
+    }],
+    'fails with TypeCheck if container is an array but index is not an integer': {
+      src: '[1] /test 0 set',
+      error: TypeCheck
+    },
+    'fails with RangeCheck if container is an array and index is out of range': [{
+      src: '[1] 1 0 set',
+      error: RangeCheck
+    }, {
+      src: '[-1] -1 0 set',
+      error: RangeCheck
+    }],
+    'fails with RangeCheck if container is a string and index is out of range': [{
+      src: '"abc" 3 0 set',
+      error: RangeCheck
+    }, {
+      src: '"abc" -1 0 set',
+      error: RangeCheck
+    }],
+    'fails with TypeCheck if container is a dictionary but index is not a name': {
+      src: 'systemdict 0 0 set',
+      error: TypeCheck
+    }
+  })
+})
---
+++ operators/set.ts
@@ -0,0 +1,77 @@
+import { Value, ValueType } from '..'
+import { RangeCheck, TypeCheck } from '../errors'
+import { State } from '../state'
+import { checkStack } from './check-state'
+import { ArrayLike } from '../objects/Array'
+import { ShareableObject } from '../objects/ShareableObject'
+import { IWritableDictionary } from '../objects/dictionaries'
+
+const setters: Record<string, (state: State) => Value> = {
+  [ValueType.string]: (state: State): Value => {
+    const [value, index, container] = state.stackRef
+    if (index.type !== ValueType.integer || value.type !== ValueType.integer) {
+      throw new TypeCheck()
+    }
+    const pos = index.data as number
+    const string = container.data as string
+    if (pos < 0 || pos >= string.length) {
+      throw new RangeCheck()
+    }
+    const parts: string[] = []
+    if (pos > 0) {
+      parts.push(string.substring(0, pos))
+    }
+    parts.push(String.fromCharCode(value.data as number))
+    if (pos + 1 < string.length) {
+      parts.push(string.substring(pos + 1))
+    }
+    return {
+      type: ValueType.string,
+      data: parts.join('')
+    }
+  },
+
+  [ValueType.array]: (state: State): Value => {
+    const [value, index, container] = state.stackRef
+    if (index.type !== ValueType.integer) {
+      throw new TypeCheck()
+    }
+    const pos = index.data as number
+    const array = container.data as ArrayLike
+    if (pos >= array.length) {
+      throw new RangeCheck()
+    }
+    array.set(pos, value)
+    return container
+  },
+
+  [ValueType.dict]: (state: State): Value => {
+    const [value, index, container] = state.stackRef
+    if (index.type !== ValueType.name) {
+      throw new TypeCheck()
+    }
+    const name = index.data as string
+    const iDict = container.data as IWritableDictionary
+    iDict.def(name, value)
+    return container
+  }
+}
+
+export function * set (state: State): Generator {
+  checkStack(state, null, null, null)
+  const container = state.stackRef[2]
+  const setter = setters[container.type]
+  if (setter === undefined) {
+    throw new TypeCheck()
+  }
+  ShareableObject.addRef(container)
+  try {
+    const value = setter(state)
+    state.pop()
+    state.pop()
+    state.pop()
+    state.push(value)
+  } finally {
+    ShareableObject.release(container)
+  }
+}
