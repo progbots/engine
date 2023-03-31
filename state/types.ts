8c523a613932e106ed644ccbcc9194ea750fc3d4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-03-30 21:47:05 -0400

    feat(valueex): extend value for debug info

--- state/types.ts
+++ state/types.ts
@@ -1,3 +1,11 @@
+import { Value } from '..'
 import { State } from './State'
 
 export type OperatorFunction = (state: State) => Generator
+
+export interface ValueEx extends Value {
+  untracked?: boolean
+  source?: string
+  sourceFile?: string
+  sourcePos?: number
+}
