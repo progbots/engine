1e56ef1d8b465f8e5bb44ca4f166cc1ece78ce09
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-03-30 22:14:40 -0400

    feat(valueex): add debug info

--- state/parser.ts
+++ state/parser.ts
@@ -1,31 +1,39 @@
-import { Value, ValueType } from '..'
+import { ValueType } from '..'
+import { ValueEx } from '.'
 
-// TODO: the output should contain position in source (for debugging purpose)
-
-export function * parse (source: string): Generator<Value> {
+export function * parse (source: string, sourceFile?: string): Generator<ValueEx> {
   const matcher = /%[^\n]*|(?:"([^"]*)")|\s+|((?:-|\+)?\d+)|\/(\S+)|(\[|\]|{|}|\S+)/g
   let match = matcher.exec(source)
   while (match !== null) {
     const [, string, integer, name, call] = match
+    const debugInfos = {
+      source,
+      sourceFile,
+      sourcePos: match.index
+    }
     if (string !== undefined) {
       yield {
         type: ValueType.string,
-        data: string
+        data: string,
+        ...debugInfos
       }
     } else if (integer !== undefined) {
       yield {
         type: ValueType.integer,
-        data: parseInt(integer, 10)
+        data: parseInt(integer, 10),
+        ...debugInfos
       }
     } else if (name !== undefined) {
       yield {
         type: ValueType.name,
-        data: name
+        data: name,
+        ...debugInfos
       }
     } else if (call !== undefined) {
       yield {
         type: ValueType.call,
-        data: call
+        data: call,
+        ...debugInfos
       }
     }
     match = matcher.exec(source)
