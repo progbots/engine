b70d9c33c423b2c53cb135d3a1e1c0bc21b46fab
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-01 14:41:57 -0400

    refactor(internalvalue): migrate code to InternalValue

--- objects/Stack.ts
+++ objects/Stack.ts
@@ -1,13 +1,12 @@
-import { Value } from '..'
-import { ValueEx } from '../state'
+import { InternalValue } from '../state'
 import { BaseArray } from './BaseArray'
 
 export class Stack extends BaseArray {
-  protected pushImpl (value: ValueEx): void {
+  protected pushImpl (value: InternalValue): void {
     this._values.unshift(value)
   }
 
-  protected popImpl (): ValueEx {
-    return this._values.shift() as Value
+  protected popImpl (): InternalValue {
+    return this._values.shift() as InternalValue
   }
 }
