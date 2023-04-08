fef73f7be5fe7db56b5899a3134df9ecc7985165
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-08 00:53:56 -0400

    fix(operators/close-array): remove TODO comment

--- operators/open-close-helper.ts
+++ operators/open-close-helper.ts
@@ -4,7 +4,7 @@ import { State } from '../state'
 import { findMarkPos } from './operands'
 
 export function openWithMark (state: State): void {
-  const openInstruction = state.callsRef[0] // TODO not working because it must go level up
+  const openInstruction = state.callsRef[0]
   state.push({
     ...openInstruction, // propagate debug infos
     type: ValueType.mark,
