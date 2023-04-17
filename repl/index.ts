8aa460289b6cbfa6fc35320824a81658a05c8d05
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-16 23:17:32 -0400

    fix(repl/node): update samples folder path

--- repl/index.ts
+++ repl/index.ts
@@ -33,7 +33,7 @@ const replHost: IReplHost = {
   },
 
   getSample (name: string) {
-    return readFileSync(join('doc/samples', name)).toString()
+    return readFileSync(join('docs/samples', name)).toString()
   }
 }
 
