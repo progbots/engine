7727ed2e16b6ab23a55afbd20b46b9fd75246231
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-18 23:34:21 -0400

    feat(operators/catch): catch impl (#7)

---
+++ operators/catch.spec.ts
@@ -0,0 +1,35 @@
+import { StackUnderflow, TypeCheck, Undefined } from '../errors/index'
+import { executeTests } from '../test-helpers'
+
+describe('operators/catch', () => {
+  executeTests({
+    'does not execute the catch block if no exception occurred': {
+      src: '{ 1 2 } { 3 } catch',
+      expect: '1 2'
+    },
+    'executes the catch block when an exception occurs': {
+      src: '{ 1 undefined 2 } { pop 3 } catch',
+      expect: '1 3'
+    },
+    'receives error details': {
+      src: '{ 1 undefined 2 } { begin type name end } catch',
+      expect: '1 "system" "Undefined"'
+    },
+    'throws the error in the catch proc': {
+      src: '{ 1 stackunderflow 2 } { /name get 3 undefined 4 } catch',
+      error: Undefined,
+      expect: '1 "StackUnderflow" 3'
+    },
+    'fails with StackUnderflow on a stack with only one proc': {
+      src: '{ } catch',
+      error: StackUnderflow
+    },
+    'fails with TypeCheck if the operands are not proc': [{
+      src: '{ } "not_a_proc" catch',
+      error: TypeCheck
+    }, {
+      src: '"not_a_proc" { } catch',
+      error: TypeCheck
+    }]
+  })
+})
---
+++ operators/catch.ts
@@ -0,0 +1,30 @@
+import { State } from '../state/index'
+import { IDictionary, ValueType } from '../index'
+import { checkOperands } from './operands'
+import { ShareableObject } from '../objects/ShareableObject'
+import { BaseError } from '../errors/BaseError'
+
+export function * catchOp (state: State): Generator {
+  const [procCatch, proc] = checkOperands(state, ValueType.proc, ValueType.proc)
+  ShareableObject.addRef([proc, procCatch])
+  try {
+    state.pop()
+    state.pop()
+    yield * state.eval(proc)
+  } catch (e) {
+    if (e instanceof BaseError) {
+      state.push({
+        type: ValueType.dict,
+        data: e as IDictionary
+      })
+    }
+    yield * state.eval(procCatch)
+  } finally {
+    ShareableObject.release([proc, procCatch])
+  }
+}
+
+Object.defineProperty(catchOp, 'name', {
+  value: 'catch',
+  writable: false
+})
