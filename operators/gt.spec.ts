380576cfbcd04d0ac2ecec206a0d78ddac871f0e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-25 23:28:28 -0400

    feat(operators/gt): gt

---
+++ operators/gt.spec.ts
@@ -0,0 +1,33 @@
+import { StackUnderflow, TypeCheck } from '../errors/index'
+import { executeTests } from '../test-helpers'
+
+describe('operators/gt', () => {
+  executeTests({
+    'outputs 1 > 1': {
+      src: '1 1 gt',
+      expect: 'false'
+    },
+    'outputs 1 > 2': {
+      src: '1 2 gt',
+      expect: 'false'
+    },
+    'outputs 2 > 1': {
+      src: '2 1 gt',
+      expect: 'true'
+    },
+    'fails with StackUnderflow on insufficient stack': [{
+      src: 'gt',
+      error: StackUnderflow
+    }, {
+      src: '1 gt',
+      error: StackUnderflow
+    }],
+    'fails with TypeCheck if not integers': [{
+      src: '"1" 2 gt',
+      error: TypeCheck
+    }, {
+      src: '1 "2" gt',
+      error: TypeCheck
+    }]
+  })
+})
