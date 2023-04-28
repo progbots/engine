5ea036f99a510b6d00117511ce543f7f9310f4a2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-27 22:31:09 -0400

    fix(operators/exch): mutation testing feedback

--- operators/exch.spec.ts
+++ operators/exch.spec.ts
@@ -3,9 +3,13 @@ import { executeTests } from '../test-helpers'
 
 describe('operators/exch', () => {
   executeTests({
-    'swap top items of the stack': {
+    'swaps top items of the stack': {
       src: '1 2 exch',
       expect: '2 1'
+    },
+    'manages reference counts': {
+      src: '[1] [2] exch aload exch aload',
+      expect: '1 2'
     },
     'fails with StackUnderflow on empty stack': {
       src: 'exch',
