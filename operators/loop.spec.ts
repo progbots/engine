e25cdeeab08763e5609ae31cf502b9a2fae36585
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-04-27 23:34:18 -0400

    fix(operators/loop): mutation testing feedback

--- operators/loop.spec.ts
+++ operators/loop.spec.ts
@@ -7,6 +7,10 @@ describe('operators/loop', () => {
       src: '{ count 5 eq { break } if 1 } loop',
       expect: '1 1 1 1 1'
     },
+    'loops until an error occurs': {
+      src: '{ typecheck } loop',
+      error: TypeCheck
+    },
     'fails with StackUnderflow on empty stack': {
       src: 'loop',
       error: StackUnderflow
