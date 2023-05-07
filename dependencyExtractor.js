99c4feae82687c51014b0ac789aea04869e7e4d3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-05-07 11:22:45 -0400

    fix(dependencyExtractor): for Windows based systems

--- dependencyExtractor.js
+++ dependencyExtractor.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+// const { writeFileSync } = require('fs')
+// const { relative } = require('path')
 
 // npx jest --findRelatedTests operators/add.ts --listTests
 // npx jest --findRelatedTests operators/integer.ts --listTests
@@ -8,10 +10,13 @@ const { join } = require('path')
 
 module.exports = {
   extract (code, filePath, defaultExtract) {
+    // const relPath = relative(__dirname, filePath)
+    // const logPath = join(__dirname, 'dependencyExtractor', relPath.replace(/\/|\\/g, '_') + '.txt')
+    // writeFileSync(logPath, filePath + '\n', { flag: 'a' })
     if (filePath.endsWith('.spec.ts')) {
       const set = new Set()
       set.add(filePath.replace('.spec.ts', '.ts'))
-      code.replace(/\/\/\s*test-for\s+([^\n]*)\n/g, (match, dependencies) => {
+      code.replace(/\/\/\s*test-for\s+([^\r\n]*)\r?\n/g, (match, dependencies) => {
         dependencies.replace(/\s*([^ ,]+),?/g, (match, dependency) => {
           set.add(join(filePath, '..', dependency))
         })
@@ -19,8 +24,10 @@ module.exports = {
       if (filePath.match(/\boperators\b/)) {
         set.add(join(filePath, '../operands.ts'))
       }
+      // writeFileSync(logPath, '\tset:' + JSON.stringify([...set.entries()], undefined, 2) + '\t', { flag: 'a' })
       return set
     }
+    // writeFileSync(logPath, '\tnot a spec\n', { flag: 'a' })
     return new Set()
   },
   getCacheKey () {
