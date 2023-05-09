eff4003d044c289edf16222f5a994b835a24fffb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-05-09 10:00:33 -0400

    refactor(state): isolate dictionary stack helpers

--- objects/stacks/Dictionary.ts
+++ objects/stacks/Dictionary.ts
@@ -2,8 +2,47 @@ import { IDictionary, ValueType } from '../../index'
 import { DictStackUnderflow, Undefined } from '../../errors/index'
 import { InternalValue } from '../../state/index'
 import { Stack } from './Stack'
+import { MemoryTracker } from '../../state/MemoryTracker'
+import { Dictionary, HostDictionary, SystemDictionary } from '../dictionaries/index'
 
 export class DictionaryStack extends Stack {
+  private readonly _globaldict: Dictionary
+  private readonly _systemdict: SystemDictionary = new SystemDictionary()
+  private readonly _minDictCount: number
+
+  constructor (tracker: MemoryTracker, hostDictionary?: IDictionary) {
+    super(tracker)
+    if (hostDictionary !== undefined) {
+      this.begin(new HostDictionary(hostDictionary))
+    }
+    this.begin(this._systemdict)
+    this._globaldict = new Dictionary(tracker)
+    this.begin(this._globaldict)
+    this._minDictCount = this._values.length
+  }
+
+  get systemdict (): SystemDictionary {
+    return this._systemdict
+  }
+
+  get globaldict (): Dictionary {
+    return this._globaldict
+  }
+
+  begin (dictionary: IDictionary): void {
+    this.push({
+      type: ValueType.dict,
+      data: dictionary
+    })
+  }
+
+  end (): void {
+    if (this.length === this._minDictCount) {
+      throw new DictStackUnderflow()
+    }
+    this.pop()
+  }
+
   lookup (name: string): InternalValue {
     for (const dictionaryValue of this._values) {
       const dictionary = dictionaryValue.data as IDictionary
@@ -13,19 +52,5 @@ export class DictionaryStack extends Stack {
       }
     }
     throw new Undefined()
-  }
-
-  begin (dictionary: IDictionary): void {
-    this._values.push({
-      type: ValueType.dict,
-      data: dictionary
-    })
-  }
-
-  end (): void {
-    if (this._values.length === this._minDictCount) {
-      throw new DictStackUnderflow()
-    }
-    this._values.pop()
   }
 }
