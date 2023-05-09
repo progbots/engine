bedd9bc7d823b8f04aa45e86a661a383d375cd05
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-05-09 06:44:29 -0400

    refactor(state): operand stack

---
+++ objects/stacks/Operand.ts
@@ -0,0 +1,34 @@
+import { ValueType } from '../../index'
+import { StackUnderflow, TypeCheck, UnmatchedMark } from '../../errors/index'
+import { InternalValue } from '../../state/index'
+import { Stack } from './Stack'
+
+export class OperandStack extends Stack {
+  check (...types: Array<ValueType | null>): InternalValue[] {
+    if (types.length > this._values.length) {
+      throw new StackUnderflow()
+    }
+    return types.map((type: ValueType | null, pos: number): InternalValue => {
+      const value = this._values[pos]
+      if (type !== null && value.type !== type) {
+        throw new TypeCheck()
+      }
+      return value
+    })
+  }
+
+  splice (count: number, ...values: InternalValue[]): void {
+    if (this._values.length < count) {
+      throw new StackUnderflow()
+    }
+    this._values.splice(0, count, ...values)
+  }
+
+  findMarkPos (): number {
+    const pos = this._values.findIndex((value: InternalValue) => value.type === ValueType.mark)
+    if (pos === -1) {
+      throw new UnmatchedMark()
+    }
+    return pos
+  }
+}
