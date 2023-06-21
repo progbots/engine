6512e05910475e736d220b5d02cbce6b77bc97f6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-06-21 10:32:13 -0400

    fix(repl): removes color mapping because of xterm use in online version

--- repl/index.ts
+++ repl/index.ts
@@ -2,7 +2,6 @@ import * as readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
 import { readSync, readFileSync } from 'node:fs'
 import { join } from 'node:path'
-import { blue, cyan, green, magenta, red, white, yellow } from './colors'
 import { main } from './impl'
 import { IReplHost } from './replHost'
 
@@ -10,16 +9,7 @@ const rl = readline.createInterface({ input, output })
 
 const replHost: IReplHost = {
   output (text) {
-    console.log(text
-      .replaceAll(red, '\x1b[31m')
-      .replaceAll(green, '\x1b[32m')
-      .replaceAll(yellow, '\x1b[33m')
-      .replaceAll(blue, '\x1b[34m')
-      .replaceAll(magenta, '\x1b[35m')
-      .replaceAll(cyan, '\x1b[36m')
-      .replaceAll(white, '\x1b[37m') +
-      '\x1b[37m'
-    )
+    console.log(text + '\x1b[37m')
   },
 
   async getInput () {
