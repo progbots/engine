40a9270c273f6f647bb7afc61a14f88c64d65bdb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-06-21 10:33:13 -0400

    documentation(repl/online): integrates xterm

--- docs/repl/index.js
+++ docs/repl/index.js
@@ -1,41 +1,22 @@
 (async function () {
   const { main } = await System.import('repl/impl')
-  const { blue, cyan, green, magenta, red, white, yellow } = await System.import('repl/colors')
+
+  /* global Terminal, LocalEchoController */
+
+  var term = new Terminal({
+    cursorBlink: 'block'
+  })
+  term.open(document.getElementById('terminal'))
+  const localEcho = new LocalEchoController()
+  term.loadAddon(localEcho)
 
   const replHost = {
     output (text) {
-      const line = document.createElement('div')
-      text.split('\u270e').forEach((part, index) => {
-        if (index === 0) {
-          if (part) {
-            line.appendChild(document.createTextNode(part))
-          }
-          return
-        }
-        const [, color, colored] = part.match(/^(red|green|yellow|blue|magenta|cyan|white)(.*)/)
-        if (colored) {
-          const span = document.createElement('span')
-          span.className = color
-          span.appendChild(document.createTextNode(colored))
-          line.appendChild(span)
-        }
-      })
-      document.body.appendChild(line).scrollIntoView()
+      term.write(text + '\r\n')
     },
   
     getInput () {
-      return new Promise (resolve => {
-        const input = document.createElement('input')
-        input.setAttribute('type', 'text')
-        document.body.appendChild(input)
-        input.focus()
-        input.addEventListener('keypress', event => {
-          if (event.key === 'Enter') {
-            resolve(input.value)
-            input.setAttribute('readonly', '')
-          }
-        })
-      })
+      return localEcho.read('? ')
     },
   
     getChar () {
