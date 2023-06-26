c313cf58e90519912770a1fe2add021380252a3f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-06-26 07:52:05 -0400

    refactor(operators/loop): use generator to loop

--- operators/flow/loop.ts
+++ operators/flow/loop.ts
@@ -2,6 +2,7 @@ import { ValueType } from '../../index'
 import { Break } from '../../errors/index'
 import { ShareableObject } from '../../objects/ShareableObject'
 import { State } from '../../state/index'
+import { InternalError } from '../../errors/InternalError'
 
 export function * loop (state: State): Generator {
   const { operands } = state
@@ -9,13 +10,18 @@ export function * loop (state: State): Generator {
   ShareableObject.addRef(block)
   try {
     operands.pop()
-    // Stryker disable next-line all: will obviously lead to a timeout
-    while (true) {
-      yield * state.evalBlockOrProc(block)
-    }
-  } catch (e) {
-    if (!(e instanceof Break)) {
-      throw e
+    let continueLoop = true
+    while (continueLoop) {
+      yield // before loop
+      state.queue({
+        block,
+        catch: (e: InternalError) => {
+          if (!(e instanceof Break)) {
+            throw e
+          }
+          continueLoop = false
+        }
+      })
     }
   } finally {
     ShareableObject.release(block)
