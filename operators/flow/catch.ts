3116cce42ff636257c8e9312c0950ca8051452bf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-06-26 07:47:18 -0400

    refactor(operators/catch): manages complexity of keeping objects alive

--- operators/flow/catch.ts
+++ operators/flow/catch.ts
@@ -3,25 +3,27 @@ import { ValueType } from '../../index'
 import { ShareableObject } from '../../objects/ShareableObject'
 import { InternalError } from '../../errors/InternalError'
 
-export function * catchOp (state: State): Generator {
+export function catchOp (state: State): void {
   const { operands } = state
   const [blockCatch, block] = operands.check(ValueType.block, ValueType.block)
   ShareableObject.addRef([block, blockCatch])
   try {
     operands.splice(2)
-    yield * state.evalBlockOrProc(block)
-  } catch (e) {
-    if (e instanceof InternalError) {
-      operands.push({
-        type: ValueType.dict,
-        data: e.dictionary
-      })
-      e.release()
-      yield * state.evalBlockOrProc(blockCatch)
-    } else {
-      // Any other error is incompatible with the engine
-      throw e
-    }
+    state.queue({
+      block,
+      catch: (e: InternalError) => {
+        operands.push({
+          type: ValueType.dict,
+          data: e.dictionary
+        })
+        e.release()
+        state.queue(blockCatch)
+      },
+      finally: () => {
+        ShareableObject.release(blockCatch)
+      }
+    })
+    ShareableObject.addRef(blockCatch)
   } finally {
     ShareableObject.release([block, blockCatch])
   }
