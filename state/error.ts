49339aa060c0815fc9ffe80a44c00e365caa5f9a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-06-30 23:45:44 -0400

    refactor(state): isolates wrapError

---
+++ state/error.ts
@@ -0,0 +1,22 @@
+import { InternalError } from '../errors/InternalError'
+import { Break, InvalidBreak } from '../errors/index'
+
+export function wrapError (error: Error): void {
+  if (error !== undefined) {
+    // No internal error should go out because memory cannot be controlled (and they are not documented)
+    if (error instanceof Break) {
+      const invalidBreak = new InvalidBreak()
+      invalidBreak.callstack = error.callstack
+      error = invalidBreak
+    }
+    // No internal error should go out because memory cannot be controlled (and they are not documented)
+    if (error instanceof InternalError) {
+      const ex = new Error(error.message)
+      ex.name = error.name
+      ex.stack = error.callstack
+      error.release()
+      throw ex
+    }
+    throw error
+  }
+}
