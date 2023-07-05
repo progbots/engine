01e4f877072c8713231e21dc2ed3cd1729ce3c34
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-07-05 07:23:04 -0400

    feat(stacks/Stack): implement splice at base level

--- objects/stacks/Stack.spec.ts
+++ objects/stacks/Stack.spec.ts
@@ -1,7 +1,8 @@
 import { Stack } from './Stack'
 import { MemoryTracker } from '../../state/MemoryTracker'
-import { ValueType } from '../../index'
+import { IArray, ValueType } from '../../index'
 import { StackUnderflow } from '../../errors/index'
+import { ShareableObject } from '../ShareableObject'
 
 class MyStack extends Stack {
   clear (): void {
@@ -9,20 +10,30 @@ class MyStack extends Stack {
   }
 }
 
+class MyObject extends ShareableObject {
+  public disposeCalled: number = 0
+
+  protected _dispose (): void {
+    ++this.disposeCalled
+  }
+}
+
 describe('objects/stacks/Stack', () => {
   let tracker: MemoryTracker
   let stack: MyStack
+  let initialUsedMemory: number
 
   beforeEach(() => {
     tracker = new MemoryTracker()
     stack = new MyStack(tracker)
+    initialUsedMemory = tracker.used
     stack.push({
-      type: ValueType.integer,
-      data: 1
+      type: ValueType.string,
+      data: 'abc'
     })
     stack.push({
       type: ValueType.integer,
-      data: 2
+      data: 123
     })
   })
 
@@ -33,23 +44,94 @@ describe('objects/stacks/Stack', () => {
   it('offers an array reference', () => {
     expect(stack.ref).toStrictEqual([{
       type: ValueType.integer,
-      data: 2
+      data: 123
     }, {
-      type: ValueType.integer,
-      data: 1
+      type: ValueType.string,
+      data: 'abc'
     }])
   })
 
   it('implements a LIFO stack (pop)', () => {
     stack.pop()
     expect(stack.ref).toStrictEqual([{
-      type: ValueType.integer,
-      data: 1
+      type: ValueType.string,
+      data: 'abc'
     }])
   })
 
   it('fails pop with StackUnderflow after all items were removed', () => {
     stack.clear()
     expect(() => stack.pop()).toThrowError(StackUnderflow)
+  })
+
+  describe('splice', () => {
+    it('removes values from the stack', () => {
+      stack.splice(2)
+      expect(tracker.used).toStrictEqual(initialUsedMemory)
+    })
+
+    describe('handling of shared objects', () => {
+      let sharedObject: MyObject
+
+      beforeEach(() => {
+        sharedObject = new MyObject()
+        stack.push({
+          type: ValueType.array,
+          data: sharedObject as unknown as IArray
+        })
+        sharedObject.release()
+        expect(sharedObject.refCount).toStrictEqual(1)
+      })
+
+      it('releases on splice', () => {
+        stack.splice(1)
+        expect(sharedObject.refCount).toStrictEqual(0)
+        expect(sharedObject.disposeCalled).toStrictEqual(1)
+      })
+
+      it('does not release if added again (1)', () => {
+        stack.splice(1, [{
+          type: ValueType.array,
+          data: sharedObject as unknown as IArray
+        }])
+        expect(sharedObject.refCount).toStrictEqual(1)
+        expect(sharedObject.disposeCalled).toStrictEqual(0)
+      })
+    })
+
+    it('removes and adds values to the stack (1)', () => {
+      stack.splice(1, {
+        type: ValueType.integer,
+        data: 456
+      })
+      expect(stack.ref).toStrictEqual([{
+        type: ValueType.integer,
+        data: 456
+      }, {
+        type: ValueType.string,
+        data: 'abc'
+      }])
+    })
+
+    it('removes and adds values to the stack (2)', () => {
+      stack.splice(2, [{
+        type: ValueType.integer,
+        data: 456
+      }, {
+        type: ValueType.string,
+        data: 'def'
+      }])
+      expect(stack.ref).toStrictEqual([{
+        type: ValueType.string,
+        data: 'def'
+      }, {
+        type: ValueType.integer,
+        data: 456
+      }])
+    })
+
+    it('fails with StackUnderflow if not enough values', () => {
+      expect(() => stack.splice(3)).toThrowError(StackUnderflow)
+    })
   })
 })
--- objects/stacks/Stack.ts
+++ objects/stacks/Stack.ts
@@ -17,4 +17,22 @@ export class Stack extends BaseArray {
     }
     super.pop()
   }
+
+  splice (count: number, values?: InternalValue | InternalValue[]): void {
+    if (this.length < count) {
+      throw new StackUnderflow()
+    }
+    const removedValues = this._values.splice(0, count)
+    if (values !== undefined) {
+      if (!Array.isArray(values)) {
+        values = [values]
+      }
+      for (const value of values) {
+        this.push(value)
+      }
+    }
+    for (const value of removedValues) {
+      this.releaseValue(value)
+    }
+  }
 }
