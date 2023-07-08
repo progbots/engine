7fe6145a2e9c478d5bdc2c9ec6883b785207692d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-07-07 23:41:07 -0400

    refactor(operators/ifelse): adapt to new State API

--- operators/flow/ifelse.ts
+++ operators/flow/ifelse.ts
@@ -2,16 +2,16 @@ import { State } from '../../state/index'
 import { ValueType } from '../../index'
 import { ShareableObject } from '../../objects/ShareableObject'
 
-export function ifelse (state: State): undefined {
+export function * ifelse (state: State): Generator {
   const { operands } = state
   const [blockElse, blockIf, condition] = operands.check(ValueType.block, ValueType.block, ValueType.boolean)
   ShareableObject.addRef([blockIf, blockElse])
   try {
     operands.splice(3)
     if (condition.data as boolean) {
-      state.callstack.push(blockIf)
+      yield * state.stackForRunning(blockIf)
     } else {
-      state.callstack.push(blockElse)
+      yield * state.stackForRunning(blockElse)
     }
   } finally {
     ShareableObject.release([blockIf, blockElse])
