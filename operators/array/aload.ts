9b1bb72052e8fcc4610eab4ca6086e5daf380f9c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-07-17 07:57:14 -0400

    refactor(operators): use new design

--- operators/array/aload.ts
+++ operators/array/aload.ts
@@ -1,23 +1,22 @@
-import { State } from '../../state/index'
+import { State, InternalValue } from '../../state/index'
 import { ValueType } from '../../index'
 import { TypeCheck } from '../../errors/index'
 import { ArrayLike } from '../../objects/Array'
+import { setOperatorAttributes } from '../attributes'
 
-export function aload ({ operands }: State): undefined {
-  const [{ type, data }] = operands.check(null)
+export function aload ({ operands }: State, [{ type, data }]: InternalValue[]): undefined {
   if (![ValueType.array, ValueType.block, ValueType.proc].includes(type)) {
     throw new TypeCheck()
   }
   const array = data as unknown as ArrayLike
   const { length } = array
-  array.addRef()
-  try {
-    operands.pop()
-    for (let index = 0; index < length; ++index) {
-      const value = array.at(index)
-      operands.push(value)
-    }
-  } finally {
-    array.release()
+  operands.pop()
+  for (let index = 0; index < length; ++index) {
+    const value = array.at(index)
+    operands.push(value)
   }
 }
+
+setOperatorAttributes(aload, {
+  typeCheck: [null]
+})
