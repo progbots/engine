15113451d8f44e60e3873f9a335198620257e498
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-07-18 07:26:27 -0400

    fix(stacks/Call): new design implementation

--- objects/stacks/Call.ts
+++ objects/stacks/Call.ts
@@ -1,27 +1,98 @@
+import { ValueType } from '../..'
 import { MemoryTracker } from '../../state/MemoryTracker'
 import { InternalValue } from '../../state/index'
 import { Stack } from './Stack'
+import { ArrayLike } from '../Array'
+import { Internal, StackUnderflow } from '../../errors/index'
 
-export enum CallStep {
+enum CallStep {
+  INIT,
   RUN,
+  LOOP,
   CATCH,
   FINALLY
 }
 
-export type CallValue = InternalValue & {
-  step?: CallStep
-  generator?: Generator
+interface CallState {
+  step: CallStep
+  parameters: null | ArrayLike
+  loopIndex: number
 }
 
 export class CallStack extends Stack {
-  public static readonly EXTRA_SIZE = MemoryTracker.POINTER_SIZE + 1
+  private readonly _states: CallState[] = []
+  public static readonly EXTRA_SIZE = MemoryTracker.POINTER_SIZE + MemoryTracker.INTEGER_SIZE + 1
 
-  push (value: CallValue): void {
-    super.push(value as InternalValue)
+  push (value: InternalValue): void {
+    super.push(value)
+    this._states.push({
+      step: CallStep.INIT,
+      parameters: null,
+      loopIndex: -1
+    })
   }
 
-  get top (): CallValue {
-    return this.at(0) as CallValue
+  pop (): void {
+    super.pop()
+    const { parameters } = this._states[0]
+    if (parameters !== null) {
+      parameters.release()
+    }
+    this._states.pop()
+  }
+
+  get top (): {
+    value: InternalValue
+    state: CallState
+  } {
+    let index: number
+    if (this.at(0).type === ValueType.integer) {
+      index = 1
+    } else {
+      index = 0
+    }
+    if (index >= this._values.length) {
+      throw new StackUnderflow()
+    }
+    return {
+      value: this.at(index),
+      state: this._states[index]
+    }
+  }
+
+  get step (): CallStep {
+    return this.top.state.step
+  }
+
+  set step (value: CallStep) {
+    this.top.state.step = value
+  }
+
+  get loopIndex (): number {
+    return this.top.state.loopIndex
+  }
+
+  set loopIndex (value: number) {
+    this.top.state.loopIndex = value
+  }
+
+  get parameters (): readonly InternalValue[] {
+    const { state } = this.top
+    const { parameters } = state
+    if (parameters !== null) {
+      return parameters.ref
+    }
+    return []
+  }
+
+  set parameters (values: InternalValue[]) {
+    const { state } = this.top
+    if (state.parameters !== null) {
+      throw new Internal('Parameters are already set')
+    }
+    const array = new ArrayLike(this.memoryTracker)
+    values.forEach(value => array.push(value))
+    state.parameters = array
   }
 
   protected addValueRef (value: InternalValue): void {
