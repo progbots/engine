c095ce9b486b4cb0c36b3cb674a1b6549f929b74
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-07-28 14:09:52 -0400

    feat(state/run): add new steps for catch / finally

--- state/run/types.ts
+++ state/run/types.ts
@@ -1,5 +1,9 @@
 import { InternalValue, State } from '../index'
 import { EngineSignal } from '../../index'
 
+export const RUN_STEP_END = -1
+export const RUN_STEP_CATCH = -2
+export const RUN_STEP_FINALLY = -3
+
 export type RunStepResult = EngineSignal | InternalValue | undefined
 export type RunSteps = Array<(this: State) => RunStepResult>
