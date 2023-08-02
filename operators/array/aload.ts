cddccef9a8ba599d88a060210bb04b497e62bb27
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-02 00:20:03 -0400

    refactor(operators): remove static cast anti pattern

--- operators/array/aload.ts
+++ operators/array/aload.ts
@@ -1,18 +1,19 @@
-import { State, InternalValue, CycleResult } from '../../state/index'
+import { State, InternalValue, CycleResult, checkIArray } from '../../state/index'
 import { ValueType } from '../../index'
 import { TypeCheck } from '../../errors/index'
-import { ArrayLike } from '../../objects/Array'
 import { setOperatorAttributes } from '../attributes'
+
+/* eslint-disable no-labels */
 
 export function aload ({ operands }: State, [{ type, data }]: readonly InternalValue[]): CycleResult {
   if (![ValueType.array, ValueType.block, ValueType.proc].includes(type)) {
     throw new TypeCheck()
   }
-  const array = data as unknown as ArrayLike
-  const { length } = array
+  assert: checkIArray(data)
+  const { length } = data
   operands.pop()
   for (let index = 0; index < length; ++index) {
-    const value = array.at(index)
+    const value = data.at(index)
     operands.push(value)
   }
   return null
--- operators/array/apop.ts
+++ operators/array/apop.ts
@@ -4,15 +4,17 @@ import { ArrayLike } from '../../objects/Array'
 import { ShareableObject } from '../../objects/ShareableObject'
 import { RangeCheck } from '../../errors/index'
 
+/* eslint-disable no-labels */
+
 export function apop ({ operands }: State): CycleResult {
   const [{ data }] = operands.check(ValueType.array)
-  const array = data as unknown as ArrayLike
-  const value = array.ref.at(-1)
+  assert: ArrayLike.check(data)
+  const value = data.ref.at(-1)
   if (value === undefined) {
     throw new RangeCheck()
   }
   ShareableObject.addRef(value) // TODO should be released *after* slice
-  array.pop()
+  data.pop()
   operands.splice(1, value)
   return null
 }
