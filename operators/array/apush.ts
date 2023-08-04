3cee218f859099358b52a713d03f9534a46d5408
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-04 00:03:20 -0400

    refactor(operators): remove static casts

--- operators/array/apush.ts
+++ operators/array/apush.ts
@@ -2,10 +2,12 @@ import { CycleResult, State } from '../../state/index'
 import { ValueType } from '../../index'
 import { ArrayLike } from '../../objects/Array'
 
+/* eslint-disable no-labels */
+
 export function apush ({ operands }: State): CycleResult {
   const [value, { data }] = operands.check(null, ValueType.array)
-  const array = data as unknown as ArrayLike
-  array.push(value)
+  assert: ArrayLike.check(data)
+  data.push(value)
   operands.splice(2)
   return null
 }
--- operators/array/join.ts
+++ operators/array/join.ts
@@ -3,14 +3,16 @@ import { TypeCheck } from '../../errors/index'
 import { CycleResult, State } from '../../state/index'
 import { ArrayLike } from '../../objects/Array'
 
+/* eslint-disable no-labels */
+
 export function join (state: State): CycleResult {
   const { operands } = state
   const [{ data }] = operands.check(ValueType.array)
-  const array = data as unknown as ArrayLike
-  if (array.some(value => value.type !== ValueType.string)) {
+  assert: ArrayLike.check(data)
+  if (data.some(value => value.type !== ValueType.string)) {
     throw new TypeCheck()
   }
-  const strings = array.ref.map(value => value.data as string)
+  const strings = data.ref.map(value => value.data as string)
   operands.splice(1, {
     type: ValueType.string,
     data: strings.join('')
