72230b7ae655253b90ee5190aefcab9a1c054d1c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-05 23:36:56 -0400

    refactor(objects/dictionaries): remove static casts

--- objects/dictionaries/types.ts
+++ objects/dictionaries/types.ts
@@ -1,14 +1,15 @@
 import { IDictionary } from '../../index'
-import { InternalValue } from '../../state/index'
+import { InternalValue, checkIDictionary } from '../../state/index'
 import { InvalidAccess } from '../../errors/index'
 
 export interface IWritableDictionary extends IDictionary {
   def: (name: string, value: InternalValue) => void
 }
 
-export function checkIWritableDictionary (dict: IDictionary): asserts dict is IWritableDictionary {
-  const method = (dict as object as Record<string, Function>).def
-  if (typeof method !== 'function' || method.length !== 2) {
+export function checkIWritableDictionary (dict: any): asserts dict is IWritableDictionary {
+  const { def } = dict
+  if (typeof def !== 'function' || def.length !== 2) {
     throw new InvalidAccess()
   }
+  checkIDictionary(dict)
 }
