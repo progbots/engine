fb94c72fa893fd730188a0f7be5967810f756f05
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-07 11:34:59 -0400

    refactor(operators): remove static casts

--- operators/generic/set.ts
+++ operators/generic/set.ts
@@ -1,50 +1,52 @@
 import { ValueType, IDictionary } from '../../index'
 import { RangeCheck, TypeCheck } from '../../errors/index'
-import { CycleResult, InternalValue, State } from '../../state/index'
+import { CycleResult, InternalValue, State, checkDictValue, checkStringValue } from '../../state/index'
 import { ArrayLike } from '../../objects/Array'
 import { checkIWritableDictionary } from '../../objects/dictionaries/index'
 
+/* eslint-disable no-labels */
+
 const setters: Record<string, (container: InternalValue, index: InternalValue, value: InternalValue) => InternalValue> = {
   [ValueType.string]: (container: InternalValue, index: InternalValue, value: InternalValue): InternalValue => {
+    assert: checkStringValue(container)
     if (index.type !== ValueType.integer || value.type !== ValueType.integer) {
       throw new TypeCheck()
     }
     const pos = index.data
-    const string = container.data as string
-    if (pos < 0 || pos >= string.length) {
+    if (pos < 0 || pos >= container.data.length) {
       throw new RangeCheck()
     }
     return {
       type: ValueType.string,
       data: [
-        string.substring(0, pos),
+        container.data.substring(0, pos),
         String.fromCharCode(value.data),
-        string.substring(pos + 1)
+        container.data.substring(pos + 1)
       ].join('')
     }
   },
 
   [ValueType.array]: (container: InternalValue, index: InternalValue, value: InternalValue): InternalValue => {
+    assert: ArrayLike.check(container.data)
     if (index.type !== ValueType.integer) {
       throw new TypeCheck()
     }
     const pos = index.data
-    const array = container.data as ArrayLike
-    if (pos >= array.length) {
+    if (pos >= container.data.length) {
       throw new RangeCheck()
     }
-    array.set(pos, value)
+    container.data.set(pos, value)
     return container
   },
 
   [ValueType.dict]: (container: InternalValue, index: InternalValue, value: InternalValue): InternalValue => {
+    assert: checkDictValue(container)
     if (index.type !== ValueType.string) {
       throw new TypeCheck()
     }
     const name = index.data
-    const iDict = container.data as IDictionary
-    checkIWritableDictionary(iDict)
-    iDict.def(name, value)
+    checkIWritableDictionary(container.data)
+    container.data.def(name, value)
     return container
   }
 }
