37a4b674c64963af65c0895e18c7da92d2ed27fa
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-07 11:28:32 -0400

    refactor(operators): remove static casts

--- operators/flow/throw.ts
+++ operators/flow/throw.ts
@@ -1,24 +1,26 @@
-import { CycleResult, State } from '../../state/index'
-import { ValueType, IDictionary } from '../../index'
+import { CycleResult, State, checkIDictionary } from '../../state/index'
+import { ValueType } from '../../index'
 import { ShareableObject } from '../../objects/ShareableObject'
 import { TypeCheck } from '../../errors/index'
 import { checkIWritableDictionary } from '../../objects/dictionaries/index'
 import { Custom } from '../../errors/Custom'
 import { InternalError } from '../../errors/InternalError'
 
+/* eslint-disable no-labels */
+
 export function throwOp ({ operands }: State): CycleResult {
-  const [dictValue] = operands.check(ValueType.dict)
-  const dict = dictValue.data as IDictionary
-  if (dict instanceof InternalError) {
-    throw dict
+  const [dict] = operands.check(ValueType.dict)
+  assert: checkIDictionary(dict.data)
+  if (dict.data instanceof InternalError) {
+    throw dict.data
   }
   try {
-    checkIWritableDictionary(dict)
+    checkIWritableDictionary(dict.data)
   } catch (e) {
     throw new TypeCheck()
   }
-  const customError = new Custom(dict) // may throw TypeCheck
-  ShareableObject.addRef(dictValue)
+  const customError = new Custom(dict.data) // may throw TypeCheck
+  ShareableObject.addRef(dict)
   operands.pop()
   throw customError
 }
