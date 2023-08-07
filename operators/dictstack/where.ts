47cae64374e61a29568dbc33a3835a9bcff4d6a6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-07 11:22:38 -0400

    refactor(operators): remove static casts

--- operators/dictstack/where.ts
+++ operators/dictstack/where.ts
@@ -1,9 +1,12 @@
 import { ValueType } from '../../index'
-import { CycleResult, State } from '../../state/index'
+import { CycleResult, State, checkStringValue } from '../../state/index'
+
+/* eslint-disable no-labels */
 
 export function where ({ operands, dictionaries }: State): CycleResult {
   const [name] = operands.check(ValueType.string)
-  const result = dictionaries.where(name.data as string)
+  assert: checkStringValue(name)
+  const result = dictionaries.where(name.data)
   if (result === null) {
     operands.splice(1, {
       type: ValueType.boolean,
