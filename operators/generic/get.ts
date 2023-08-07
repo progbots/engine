594fcc5e6e1ab1b0de5008047bbfda50fb476821
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-07 11:28:44 -0400

    refactor(operators): remove static casts

--- operators/generic/get.ts
+++ operators/generic/get.ts
@@ -1,24 +1,27 @@
-import { IDictionary, ValueType } from '../../index'
+import { ValueType } from '../../index'
 import { RangeCheck, TypeCheck, Undefined } from '../../errors/index'
-import { CycleResult, InternalValue, State } from '../../state/index'
+import { CycleResult, InternalValue, State, checkDictValue, checkStringValue } from '../../state/index'
 import { ArrayLike } from '../../objects/Array'
 
+/* eslint-disable no-labels */
+
 function arrayLikeGetter (container: InternalValue, index: InternalValue): InternalValue {
+  assert: ArrayLike.check(container.data)
   if (index.type !== ValueType.integer) {
     throw new TypeCheck()
   }
   const pos = index.data
-  const array = container.data as ArrayLike
-  return array.at(pos)
+  return container.data.at(pos)
 }
 
 const getters: Record<string, (container: InternalValue, index: InternalValue) => InternalValue> = {
   [ValueType.string]: (container: InternalValue, index: InternalValue): InternalValue => {
+    assert: checkStringValue(container)
     if (index.type !== ValueType.integer) {
       throw new TypeCheck()
     }
     const pos = index.data
-    const string = container.data as string
+    const string = container.data
     if (pos < 0 || pos >= string.length) {
       throw new RangeCheck()
     }
@@ -31,12 +34,12 @@ const getters: Record<string, (container: InternalValue, index: InternalValue) =
   [ValueType.array]: arrayLikeGetter,
 
   [ValueType.dict]: (container: InternalValue, index: InternalValue): InternalValue => {
+    assert: checkDictValue(container)
     if (index.type !== ValueType.string) {
       throw new TypeCheck()
     }
     const name = index.data
-    const iDict = container.data as IDictionary
-    const value = iDict.lookup(name)
+    const value = container.data.lookup(name)
     if (value === null) {
       throw new Undefined()
     }
