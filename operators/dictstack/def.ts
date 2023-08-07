4e67964708e1e1edf4f697d9e8ebbd5c1f4bb118
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-07 11:19:20 -0400

    refactor(operators): remove static casts

--- operators/dictstack/def.ts
+++ operators/dictstack/def.ts
@@ -1,19 +1,22 @@
-import { ValueType, IDictionary } from '../../index'
+import { ValueType } from '../../index'
 import { checkIWritableDictionary } from '../../objects/dictionaries/index'
-import { CycleResult, State } from '../../state/index'
+import { CycleResult, State, checkIDictionary, checkStringValue } from '../../state/index'
+
+/* eslint-disable no-labels */
 
 export function def ({ operands, dictionaries }: State): CycleResult {
   const [value, name] = operands.check(null, ValueType.string)
-  const { data: top } = dictionaries.at(0)
-  const topDict = top as IDictionary
+  const { data: topDict } = dictionaries.at(0) // TODO might be interesting to introduce a DictValue method
+  assert: checkStringValue(name)
+  assert_top_dict: checkIDictionary(topDict)
   checkIWritableDictionary(topDict)
   if (value.type === ValueType.block) {
-    topDict.def(name.data as string, {
+    topDict.def(name.data, {
       ...value,
       type: ValueType.proc
     })
   } else {
-    topDict.def(name.data as string, value)
+    topDict.def(name.data, value)
   }
   operands.splice(2)
   return null
