32b8e9008848cb48bba79425061aaa97c10b72e2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-15 05:31:06 -0400

    refactor(operators/attributes): externalize ts-helpers

--- src/operators/attributes.ts
+++ src/operators/attributes.ts
@@ -1,6 +1,5 @@
 import { OperatorAttributes, OperatorFunction } from '../state/types'
-
-const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>
+import { getKeys } from '../ts-helpers'
 
 export function setOperatorAttributes (operator: OperatorFunction, attributes: OperatorAttributes): void {
   Object.defineProperties(operator, getKeys(attributes).reduce((
