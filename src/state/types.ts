71b0474a94721254614948ff7802c5d037eded9e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-18 09:17:52 -0400

    refactor(state/types): adapts to new Value

--- src/state/types.ts
+++ src/state/types.ts
@@ -1,6 +1,30 @@
-import { InternalError } from '../src/errors/InternalError'
-import { BlockValue, BooleanValue, CallValue, DictValue, EngineSignal, IArray, IDictionary, IntegerValue, OperatorValue, StringValue, Value, ValueType } from '../index'
-import { State } from './State'
+import {
+  ArrayValue,
+  BlockValue,
+  BooleanValue,
+  CallValue,
+  DictionaryValue,
+  IArray,
+  IDictionary,
+  IState,
+  IntegerValue,
+  OperatorValue,
+  Signal,
+  StringValue,
+  Value,
+  ValueType,
+  checkArrayValue,
+  checkBlockValue,
+  checkBooleanValue,
+  checkCallValue,
+  checkDictionaryValue,
+  checkIntegerValue,
+  checkOperatorValue,
+  checkStringValue,
+  throwValueIsNotOfType
+} from '../index'
+import { InternalError } from '../errors/InternalError'
+import { OperandStack } from '../objects/stacks'
 
 export interface DebugInfos {
   source?: string // Source code
@@ -14,7 +38,99 @@ type Internal<T> = T & DebugInfos & {
 
 export type InternalValue = Internal<Value>
 
-export type CycleResult = EngineSignal | InternalValue | null
+export type CycleResult = Signal | InternalValue | null
+
+export interface IInternalState extends IState {
+  operands: OperandStack
+}
+
+const INVALID_VALUE = 'Invalid Value'
+
+function scanGenericValue (value: any): asserts value is Value {
+  if (typeof value !== 'object') {
+    throw new InternalError(INVALID_VALUE)
+  }
+  const { type } = value
+  if (!Object.values(ValueType).includes(type)) {
+    throw new InternalError(INVALID_VALUE)
+  }
+}
+
+function buildScan<T> (
+  type: ValueType,
+  check: (value: any) => asserts value is T,
+  scan: (value: T) => boolean
+): (value: any) => asserts value is T {
+  return function (value: any) {
+    try {
+      scanGenericValue(value)
+      check(value)
+      if (!scan(value)) {
+        throw Error()
+      }
+    } catch (e) {
+      throwValueIsNotOfType(type)
+    }
+  }
+}
+
+export const scanBooleanValue = buildScan<BooleanValue>(
+  ValueType.boolean,
+  checkBooleanValue,
+  (value) => {
+    const { isSet } = value
+    return typeof isSet === 'boolean'
+  }
+)
+
+export const scanIntegerValue = buildScan<IntegerValue>(
+  ValueType.integer,
+  checkIntegerValue,
+  (value) => {
+    const { number } = value
+    return typeof number === 'number'
+  }
+)
+
+export const scanStringValue = buildScan<StringValue>(
+  ValueType.string,
+  checkStringValue,
+  (value) => {
+    const { string } = value
+    return typeof string === 'string'
+  }
+)
+
+const INVALID_IARRAY = 'Invalid IArray'
+
+export function checkIArray (value: any): asserts value is IArray {
+  if (typeof value !== 'object') {
+    throw new InternalError(INVALID_IARRAY)
+  }
+  const { length, at } = value
+  if (typeof length !== 'number' || length < 0 || typeof at !== 'function' || at.length !== 1) {
+    throw new InternalError(INVALID_IARRAY)
+  }
+}
+
+export const scanBlockValue = buildScan<BlockValue>(
+  ValueType.block,
+  checkBlockValue,
+  (value) => {
+    const { block } = value
+    checkIArray(block)
+    return true
+  }
+)
+
+export const scanCallValue = buildScan<CallValue>(
+  ValueType.call,
+  checkCallValue,
+  (value) => {
+    const { call } = value
+    return typeof call === 'string'
+  }
+)
 
 export interface OperatorAttributes {
   name?: string
@@ -22,30 +138,19 @@ export interface OperatorAttributes {
   // When specified, the collected values are kept valid during the operator lifetime (including catch & finally)
   typeCheck?: Array<ValueType | null>
   // When specified, the method is called until the result is false
-  loop?: (state: State, parameters: readonly InternalValue[], index: number) => CycleResult | false
+  loop?: (state: IInternalState, parameters: readonly InternalValue[], index: number) => CycleResult | false
   // When specified, any InternalError is transmitted to it
-  catch?: (state: State, parameters: readonly InternalValue[], e: InternalError) => CycleResult
+  catch?: (state: IInternalState, parameters: readonly InternalValue[], e: InternalError) => CycleResult
   // When specified, triggered before unstacking the operator from call stack
-  finally?: (state: State, parameters: readonly InternalValue[]) => CycleResult
+  finally?: (state: IInternalState, parameters: readonly InternalValue[]) => CycleResult
 }
 
 export interface OperatorFunction extends OperatorAttributes {
-  (state: State, parameters: readonly InternalValue[]): CycleResult
+  (state: IInternalState, parameters: readonly InternalValue[]): CycleResult
   name: string
 }
 
-export function isA<T> (checker: (value: any) => asserts value is T): (value: any) => value is T {
-  return function (value: any): value is T {
-    try {
-      checker(value)
-      return true
-    } catch (e) {
-      return false
-    }
-  }
-}
-
-const NOT_AN_OPERATOR_FUNCTION = 'Not an OperatorFunction'
+const INVALID_OPERATOR_FUNCTION = 'Invalid OperatorFunction'
 
 export function checkOperatorFunction (value: any): asserts value is OperatorFunction {
   const { name, constant, typeCheck, loop, catch: catchFunc, finally: finallyFunc } = value
@@ -54,7 +159,7 @@ export function checkOperatorFunction (value: any): asserts value is OperatorFun
       throw new Error()
     }
     if (constant !== undefined) {
-      checkGenericValue(constant)
+      scanGenericValue(constant)
     }
     if (typeCheck !== undefined &&
       (!Array.isArray(typeCheck) || typeCheck.some(value => value !== null && !Object.values(ValueType).includes(value)))) {
@@ -66,121 +171,48 @@ export function checkOperatorFunction (value: any): asserts value is OperatorFun
       throw new Error()
     }
   } catch (e) {
-    throw new InternalError(NOT_AN_OPERATOR_FUNCTION)
+    throw new InternalError(INVALID_OPERATOR_FUNCTION)
   }
 }
 
-const NOT_A_VALUE = 'Not a Value'
-
-export function checkGenericValue (value: any): asserts value is Value {
-  if (typeof value !== 'object') {
-    throw new InternalError(NOT_A_VALUE)
+export const scanOperatorValue = buildScan<OperatorValue>(
+  ValueType.operator,
+  checkOperatorValue,
+  (value) => {
+    const { operator } = value
+    checkOperatorFunction(operator)
+    return true
   }
-  const { type, data } = value
-  if (!Object.values(ValueType).includes(type) || data === undefined) {
-    throw new InternalError(NOT_A_VALUE)
+)
+
+export const scanArrayValue = buildScan<ArrayValue>(
+  ValueType.array,
+  checkArrayValue,
+  (value) => {
+    const { array } = value
+    checkIArray(array)
+    return true
   }
-}
+)
 
-const NOT_A_BOOLEAN_VALUE = 'Not a BooleanValue'
-
-export function checkBooleanValue (value: any): asserts value is BooleanValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.boolean || typeof data !== 'boolean') {
-    throw new InternalError(NOT_A_BOOLEAN_VALUE)
-  }
-}
-export const isBooleanValue = isA(checkBooleanValue)
-
-const NOT_AN_INTEGER_VALUE = 'Not an IntegerValue'
-
-export function checkIntegerValue (value: any): asserts value is IntegerValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.integer || typeof data !== 'number') {
-    throw new InternalError(NOT_AN_INTEGER_VALUE)
-  }
-}
-export const isIntegerValue = isA(checkIntegerValue)
-
-const NOT_A_STRING_VALUE = 'Not a StringValue'
-
-export function checkStringValue (value: any): asserts value is StringValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.string || typeof data !== 'string') {
-    throw new InternalError(NOT_A_STRING_VALUE)
-  }
-}
-export const isStringValue = isA(checkStringValue)
-
-const NOT_A_CALL_VALUE = 'Not a CallValue'
-
-export function checkCallValue (value: any): asserts value is CallValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.call || typeof data !== 'string' || data === '') {
-    throw new InternalError(NOT_A_CALL_VALUE)
-  }
-}
-export const isCallValue = isA(checkCallValue)
-
-const NOT_AN_OPERATOR_VALUE = 'Not an OperatorValue'
-
-export function checkOperatorValue (value: any): asserts value is OperatorValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.operator || typeof data !== 'function') {
-    throw new InternalError(NOT_AN_OPERATOR_VALUE)
-  }
-}
-export const isOperatorValue = isA(checkOperatorValue)
-
-const NOT_AN_IARRAY = 'Not an IArray'
-
-export function checkIArray (value: any): asserts value is IArray {
-  if (typeof value !== 'object') {
-    throw new InternalError(NOT_AN_IARRAY)
-  }
-  const { length, at } = value
-  if (typeof length !== 'number' || length < 0 || typeof at !== 'function' || at.length !== 1) {
-    throw new InternalError(NOT_AN_IARRAY)
-  }
-}
-
-const NOT_A_BLOCK_VALUE = 'Not a BlockValue'
-
-export function checkBlockValue (value: any): asserts value is BlockValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.block) {
-    throw new InternalError(NOT_A_BLOCK_VALUE)
-  }
-  checkIArray(data)
-}
-export const isBlockValue = isA(checkBlockValue)
-
-const NOT_AN_IDICTIONARY = 'Not an IDictionary'
+const INVALID_IDICTIONARY = 'Not an IDictionary'
 
 export function checkIDictionary (value: any): asserts value is IDictionary {
   if (typeof value !== 'object') {
-    throw new InternalError(NOT_AN_IDICTIONARY)
+    throw new InternalError(INVALID_IDICTIONARY)
   }
   const { names, lookup } = value
   if (!Array.isArray(names) || !names.every(name => typeof name === 'string') || typeof lookup !== 'function' || lookup.length !== 1) {
-    throw new InternalError(NOT_AN_IDICTIONARY)
+    throw new InternalError(INVALID_IDICTIONARY)
   }
 }
 
-const NOT_A_DICT_VALUE = 'Not a DictValue'
-
-export function checkDictValue (value: any): asserts value is DictValue {
-  checkGenericValue(value)
-  const { type, data } = value
-  if (type !== ValueType.dict) {
-    throw new InternalError(NOT_A_DICT_VALUE)
+export const scanDictionaryValue = buildScan<DictionaryValue>(
+  ValueType.dictionary,
+  checkDictionaryValue,
+  (value) => {
+    const { dictionary } = value
+    checkIDictionary(dictionary)
+    return true
   }
-  checkIDictionary(data)
-}
-export const isDictValue = isA(checkDictValue)
+)
