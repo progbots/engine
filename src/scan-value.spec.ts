744de71ceb61a9995039be6a127b0f49e44cb6c6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-20 00:40:49 -0400

    refactor(scan-value): for external sources

---
+++ src/scan-value.spec.ts
@@ -0,0 +1,167 @@
+import { IArray, IDictionary, Value, ValueType } from './index'
+import { checkBlockValue, checkBooleanValue, checkCallValue, checkDictValue, checkIntegerValue, checkOperatorValue, checkStringValue, isBlockValue, isBooleanValue, isCallValue, isDictValue, isIntegerValue, isOperatorValue, isStringValue } from './scan-value'
+import { executeCheckTests, checkTestsParameters } from './test/test-helpers'
+import { add } from './operators'
+
+const invalidData = [
+  undefined,
+  null,
+  true, false,
+  -1, 0, 1,
+  '', 'hello world !',
+  {},
+  add
+]
+
+function typecheck ({ name, check, is, ok, ko }: checkTestsParameters, type: ValueType): void {
+  executeCheckTests({
+    name,
+    check,
+    is,
+    ok: ok.map(data => ({ type, data })),
+    ko: invalidData
+      .filter(data => !ok.includes(data))
+      .map(data => ({
+        type,
+        data
+      }))
+      .concat(ko)
+  })
+}
+
+const iarray: IArray = {
+  length: 1,
+  at (index: number): Value {
+    return {
+      type: ValueType.integer,
+      data: index
+    }
+  }
+}
+
+const notIArrays = [{
+  length: -1,
+  at (index: number): Value {
+    return {
+      type: ValueType.integer,
+      data: index
+    }
+  }
+}, {
+  length: '123',
+  at (index: number): Value {
+    return {
+      type: ValueType.integer,
+      data: index
+    }
+  }
+}, {
+  at (index: number): Value {
+    return {
+      type: ValueType.integer,
+      data: index
+    }
+  }
+}, {
+  length: 1,
+  at (): void { }
+}, {
+}]
+
+const idictionary: IDictionary = {
+  names: ['a', 'b'],
+  lookup (name: string): Value {
+    return {
+      type: ValueType.string,
+      data: name
+    }
+  }
+}
+
+const notIDictionaries = [{
+  names: -1,
+  lookup (name: string): Value {
+    return {
+      type: ValueType.string,
+      data: name
+    }
+  }
+}, {
+  names: 'abc',
+  lookup (name: string): Value {
+    return {
+      type: ValueType.string,
+      data: name
+    }
+  }
+}, {
+  lookup (name: string): Value {
+    return {
+      type: ValueType.string,
+      data: name
+    }
+  }
+}, {
+  names: ['a', 'b'],
+  lookup (): void {
+  }
+}, {
+}]
+
+describe('state/types', () => {
+  typecheck({
+    name: 'BooleanValue',
+    check: checkBooleanValue,
+    is: isBooleanValue,
+    ok: [true, false],
+    ko: []
+  }, ValueType.boolean)
+
+  typecheck({
+    name: 'IntegerValue',
+    check: checkIntegerValue,
+    is: isIntegerValue,
+    ok: [-1, 0, 1],
+    ko: []
+  }, ValueType.integer)
+
+  typecheck({
+    name: 'StringValue',
+    check: checkStringValue,
+    is: isStringValue,
+    ok: ['', '0, 1', 'hello world !'],
+    ko: []
+  }, ValueType.string)
+
+  typecheck({
+    name: 'CallValue',
+    check: checkCallValue,
+    is: isCallValue,
+    ok: ['add', '{', 'hello world !'],
+    ko: []
+  }, ValueType.call)
+
+  typecheck({
+    name: 'OperatorValue',
+    check: checkOperatorValue,
+    is: isOperatorValue,
+    ok: [add],
+    ko: []
+  }, ValueType.operator)
+
+  typecheck({
+    name: 'BlockValue',
+    check: checkBlockValue,
+    is: isBlockValue,
+    ok: [iarray],
+    ko: notIArrays
+  }, ValueType.block)
+
+  typecheck({
+    name: 'DictValue',
+    check: checkDictValue,
+    is: isDictValue,
+    ok: [idictionary],
+    ko: notIDictionaries
+  }, ValueType.dict)
+})
---
+++ src/scan-value.ts
@@ -0,0 +1,180 @@
+import {
+  ArrayValue,
+  BlockValue,
+  BooleanValue,
+  CallValue,
+  DictionaryValue,
+  IArray,
+  IDictionary,
+  IntegerValue,
+  OperatorValue,
+  Signal,
+  StringValue,
+  Value,
+  ValueType,
+  checkArrayValue,
+  checkBlockValue,
+  checkBooleanValue,
+  checkCallValue,
+  checkDictionaryValue,
+  checkIntegerValue,
+  checkOperatorValue,
+  checkStringValue,
+  throwValueIsNotOfType
+} from './index'
+import { InternalError } from './errors/InternalError'
+
+const INVALID_VALUE = 'Invalid Value'
+
+function scanGenericValue (value: any): asserts value is Value {
+  if (typeof value !== 'object') {
+    throw new InternalError(INVALID_VALUE)
+  }
+  const { type } = value
+  if (!Object.values(ValueType).includes(type)) {
+    throw new InternalError(INVALID_VALUE)
+  }
+}
+
+function buildScan<T> (
+  type: ValueType,
+  check: (value: any) => asserts value is T,
+  scan: (value: T) => boolean
+): (value: any) => asserts value is T {
+  return function (value: any) {
+    try {
+      scanGenericValue(value)
+      check(value)
+      if (!scan(value)) {
+        throw Error()
+      }
+    } catch (e) {
+      throwValueIsNotOfType(type)
+    }
+  }
+}
+
+export const scanBooleanValue = buildScan<BooleanValue>(
+  ValueType.boolean,
+  checkBooleanValue,
+  (value) => {
+    const { isSet } = value
+    return typeof isSet === 'boolean'
+  }
+)
+
+export const scanIntegerValue = buildScan<IntegerValue>(
+  ValueType.integer,
+  checkIntegerValue,
+  (value) => {
+    const { number } = value
+    return typeof number === 'number'
+  }
+)
+
+export const scanStringValue = buildScan<StringValue>(
+  ValueType.string,
+  checkStringValue,
+  (value) => {
+    const { string } = value
+    return typeof string === 'string'
+  }
+)
+
+const INVALID_IARRAY = 'Invalid IArray'
+
+export function checkIArray (value: any): asserts value is IArray {
+  if (typeof value !== 'object') {
+    throw new InternalError(INVALID_IARRAY)
+  }
+  const { length, at } = value
+  if (typeof length !== 'number' || length < 0 || typeof at !== 'function' || at.length !== 1) {
+    throw new InternalError(INVALID_IARRAY)
+  }
+}
+
+export const scanBlockValue = buildScan<BlockValue>(
+  ValueType.block,
+  checkBlockValue,
+  (value) => {
+    const { block } = value
+    checkIArray(block)
+    return true
+  }
+)
+
+export const scanCallValue = buildScan<CallValue>(
+  ValueType.call,
+  checkCallValue,
+  (value) => {
+    const { call } = value
+    return typeof call === 'string'
+  }
+)
+
+const INVALID_OPERATOR_FUNCTION = 'Invalid OperatorFunction'
+
+export function checkOperatorFunction (value: any): asserts value is OperatorFunction {
+  const { name, constant, typeCheck, loop, catch: catchFunc, finally: finallyFunc } = value
+  try {
+    if (typeof value !== 'function' || typeof name !== 'string') {
+      throw new Error()
+    }
+    if (constant !== undefined) {
+      scanGenericValue(constant)
+    }
+    if (typeCheck !== undefined &&
+      (!Array.isArray(typeCheck) || typeCheck.some(value => value !== null && !Object.values(ValueType).includes(value)))) {
+      throw new Error()
+    }
+    if ((loop !== undefined && typeof loop !== 'function') ||
+      (catchFunc !== undefined && typeof catchFunc !== 'function') ||
+      (finallyFunc !== undefined && typeof finallyFunc !== 'function')) {
+      throw new Error()
+    }
+  } catch (e) {
+    throw new InternalError(INVALID_OPERATOR_FUNCTION)
+  }
+}
+
+export const scanOperatorValue = buildScan<OperatorValue>(
+  ValueType.operator,
+  checkOperatorValue,
+  (value) => {
+    const { operator } = value
+    checkOperatorFunction(operator)
+    return true
+  }
+)
+
+export const scanArrayValue = buildScan<ArrayValue>(
+  ValueType.array,
+  checkArrayValue,
+  (value) => {
+    const { array } = value
+    checkIArray(array)
+    return true
+  }
+)
+
+const INVALID_IDICTIONARY = 'Not an IDictionary'
+
+export function checkIDictionary (value: any): asserts value is IDictionary {
+  if (typeof value !== 'object') {
+    throw new InternalError(INVALID_IDICTIONARY)
+  }
+  const { names, lookup } = value
+  if (!Array.isArray(names) || !names.every(name => typeof name === 'string') || typeof lookup !== 'function' || lookup.length !== 1) {
+    throw new InternalError(INVALID_IDICTIONARY)
+  }
+}
+
+export const scanDictionaryValue = buildScan<DictionaryValue>(
+  ValueType.dictionary,
+  checkDictionaryValue,
+  (value) => {
+    const { dictionary } = value
+    checkIDictionary(dictionary)
+    return true
+  }
+)
