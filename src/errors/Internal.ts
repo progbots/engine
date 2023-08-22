dbd2f0b38aeb5d2fc5a7ed42aa988f34eb738356
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-22 12:52:25 -0400

    fix(errors/Internal): respects typescript constraints

--- src/errors/Internal.ts
+++ src/errors/Internal.ts
@@ -3,5 +3,5 @@ import { InternalError } from './InternalError'
 export class Internal extends InternalError {
   // For internal error, we ignore state stack
   // eslint-disable-next-line accessor-pairs
-  set callstack (value: string) {}
+  override set callstack (value: string) {}
 }
