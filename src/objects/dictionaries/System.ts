b0ef3ccec5f688929f5c3e61f6de9c57ab56322a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-24 06:27:46 -0400

    fix(dictionaries/system): temporary fix

--- src/objects/dictionaries/System.ts
+++ src/objects/dictionaries/System.ts
@@ -1,13 +1,13 @@
-import { Value, ValueType, IDictionary } from '../../index'
-import { ShareableObject } from '../ShareableObject'
-import { OperatorFunction } from '../../state/index'
-import * as operatorFunctions from '../../operators/index'
-import * as errorFunctions from '../../operators/errors'
+import { Value, ValueType, IDictionary } from '@api'
+import { IOperatorFunction } from '@sdk'
+import { ShareableObject } from '@objects/ShareableObject'
+import * as operatorFunctions from '@operators'
+import * as errorFunctions from '@operators/errors'
 
-const operators: Record<string, OperatorFunction> = {}
+const operators: Record<string, IOperatorFunction> = {}
 
 Object.values(operatorFunctions).forEach(operator => {
-  operators[operator.name] = operator
+  operators[operator.name] = operator as IOperatorFunction // TODO: remove static cast
 })
 Object.values(errorFunctions.default).forEach(operator => {
   operators[operator.name] = operator
