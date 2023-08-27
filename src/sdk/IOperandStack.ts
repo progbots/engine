72c13edd72013757f4ffaf7a243709bb6fc872df
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-27 16:59:54 -0400

    feat(sdk/IOperandStack): improve check signature

--- src/sdk/IOperandStack.ts
+++ src/sdk/IOperandStack.ts
@@ -1,9 +1,20 @@
-import { ValueType } from '@api'
+import { ArrayValue, BlockValue, BooleanValue, CallValue, DictionaryValue, IntegerValue, OperatorValue, StringValue, ValueType } from '@api'
 import { IStack } from './IStack'
-import { InternalValue } from './InternalValue'
+import { Internal, InternalValue } from './InternalValue'
 
 export interface IOperandStack extends IStack {
   splice: (count: number, values?: InternalValue | InternalValue[]) => void
-  check: (...types: Array<ValueType | null>) => readonly InternalValue[]
+  check: ((type: ValueType.boolean) => readonly [Internal<BooleanValue>]) &
+  ((type: ValueType.integer) => readonly [Internal<IntegerValue>]) &
+  ((type: ValueType.string) => readonly [Internal<StringValue>]) &
+  ((type: ValueType.block) => readonly [Internal<BlockValue>]) &
+  ((type: ValueType.call) => readonly [Internal<CallValue>]) &
+  ((type: ValueType.operator) => readonly [Internal<OperatorValue>]) &
+  ((type: ValueType.array) => readonly [Internal<ArrayValue>]) &
+  ((type: ValueType.dictionary) => readonly [Internal<DictionaryValue>]) &
+  ((type: null) => readonly [InternalValue]) &
+  ((type1: ValueType | null, type2: ValueType | null) => readonly [InternalValue, InternalValue]) &
+  ((type1: ValueType | null, type2: ValueType | null, type3: ValueType | null) => readonly [InternalValue, InternalValue, InternalValue]) &
+  ((...types: Array<ValueType | null>) => readonly InternalValue[])
   findMarkPos: () => number
 }
