eb233ddcb2aa38af7970568fce370aba1213febe
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-27 22:06:07 -0400

    fix(stacks/OperandStack): update signature

--- src/objects/stacks/OperandStack.ts
+++ src/objects/stacks/OperandStack.ts
@@ -1,10 +1,21 @@
-import { ValueType } from '@api'
-import { IOperandStack, InternalValue } from '@sdk'
+import { ArrayValue, BlockValue, BooleanValue, CallValue, DictionaryValue, IntegerValue, OperatorValue, StringValue, ValueType } from '@api'
+import { IOperandStack, Internal, InternalValue } from '@sdk'
 import { StackUnderflow, TypeCheck, UnmatchedMark } from '@errors'
 import { ValueStack } from './ValueStack'
 
 export class OperandStack extends ValueStack implements IOperandStack {
-  check (...types: Array<ValueType | null>): InternalValue[] {
+  check (type: ValueType.boolean): readonly [Internal<BooleanValue>]
+  check (type: ValueType.integer): readonly [Internal<IntegerValue>]
+  check (type: ValueType.string): readonly [Internal<StringValue>]
+  check (type: ValueType.block): readonly [Internal<BlockValue>]
+  check (type: ValueType.call): readonly [Internal<CallValue>]
+  check (type: ValueType.operator): readonly [Internal<OperatorValue>]
+  check (type: ValueType.array): readonly [Internal<ArrayValue>]
+  check (type: ValueType.dictionary): readonly [Internal<DictionaryValue>]
+  check (type: null): readonly [InternalValue]
+  check (type1: ValueType | null, type2: ValueType | null): readonly [InternalValue, InternalValue]
+  check (type1: ValueType | null, type2: ValueType | null, type3: ValueType | null): readonly [InternalValue, InternalValue, InternalValue]
+  check (...types: Array<ValueType | null>): readonly InternalValue[] {
     if (types.length > this._values.length) {
       throw new StackUnderflow()
     }
