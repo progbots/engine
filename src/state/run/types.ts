f03be9dfd686bbf8edb1bff63d8583d09ff4534d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-29 07:10:37 -0400

    refactor(run): removed

--- src/state/run/types.ts
+++
@@ -1,7 +0,0 @@
-import { InternalValue, CycleResult, State } from '../index'
-
-export const RUN_STEP_END = -1
-export const RUN_STEP_CATCH = -2
-export const RUN_STEP_FINALLY = -3
-
-export type RunSteps = Array<(this: State, top: InternalValue, index: number) => CycleResult>
