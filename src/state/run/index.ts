df533d365debce0b10b6299ec64fa602186f280e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-29 07:11:37 -0400

    refactor(run): reorganize files

--- src/state/run/index.ts
+++ src/state/run/index.ts
@@ -1,7 +1,5 @@
-import { blocktype } from './blocktype'
-
-export * from './types'
+export * from './RunSteps'
 export * from './stringtype'
 export * from './calltype'
 export * from './blocktype'
-export const proctype = blocktype
+export * from './operatortype'
