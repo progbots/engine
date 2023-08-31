ddcc41b4e25aefc3f259d3347a0fefb74b8de51b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-08-31 07:55:38 -0400

    test(calltype): wip

--- src/state/run/calltype.spec.ts
+++ src/state/run/calltype.spec.ts
@@ -2,12 +2,23 @@ import { SignalType, ValueType } from '@api'
 import { executeRunTests } from '@test/run/execute'
 import { calltype } from './calltype'
 import { Undefined } from '@errors'
+import { toIArray } from '@test/toIArray'
+import { DictionaryMapping } from '@test/toIDictionary'
 
 const LOOKUP = 1
 const AFTER = 2
 
-const host = {
-  
+const host: DictionaryMapping = {
+  integer: 1,
+  string: '2',
+  call: {
+    type: ValueType.call,
+    call: 'help'
+  },
+  block: {
+    type: ValueType.block,
+    block: toIArray([])
+  }
 }
 
 describe('state/run/calltype', () => {
@@ -47,9 +58,41 @@ describe('state/run/calltype', () => {
       callStack: [{
         type: ValueType.call,
         call: 'nope'
-      }],
+      }]
     },
     error: Undefined
+  }, {
+    before: {
+      step: LOOKUP,
+      host,
+      callStack: [{
+        type: ValueType.call,
+        call: 'integer'
+      }]
+    },
+    after: {
+      step: AFTER,
+      operands: [{
+        type: ValueType.integer,
+        number: 1
+      }]
+    }
+  }, {
+    before: {
+      step: LOOKUP,
+      host,
+      callStack: [{
+        type: ValueType.call,
+        call: 'string'
+      }]
+    },
+    after: {
+      step: AFTER,
+      operands: [{
+        type: ValueType.string,
+        string: '2'
+      }]
+    }
   }, {
     before: {
       step: AFTER,
