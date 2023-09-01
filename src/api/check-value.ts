1baebe06dd8f5ebda226cbafe18b91295c992c03
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-01 08:28:38 -0400

    feat(api): includes undefined in the checkers

--- src/api/check-value.ts
+++ src/api/check-value.ts
@@ -14,40 +14,40 @@ export function throwValueIsNotOfType (expectedType: ValueType): never {
   throw new InternalError(`Value is not of type '${expectedType}`)
 }
 
-function check (value: Value, expectedType: ValueType): void {
-  if (value.type !== expectedType) {
+function check (value: Value | undefined, expectedType: ValueType): void {
+  if (value === undefined || value.type !== expectedType) {
     throwValueIsNotOfType(expectedType)
   }
 }
 
-export function checkBooleanValue (value: Value): asserts value is BooleanValue {
+export function checkBooleanValue (value: Value | undefined): asserts value is BooleanValue {
   check(value, ValueType.boolean)
 }
 
-export function checkIntegerValue (value: Value): asserts value is IntegerValue {
+export function checkIntegerValue (value: Value | undefined): asserts value is IntegerValue {
   check(value, ValueType.integer)
 }
 
-export function checkStringValue (value: Value): asserts value is StringValue {
+export function checkStringValue (value: Value | undefined): asserts value is StringValue {
   check(value, ValueType.string)
 }
 
-export function checkBlockValue (value: Value): asserts value is BlockValue {
+export function checkBlockValue (value: Value | undefined): asserts value is BlockValue {
   check(value, ValueType.block)
 }
 
-export function checkCallValue (value: Value): asserts value is CallValue {
+export function checkCallValue (value: Value | undefined): asserts value is CallValue {
   check(value, ValueType.call)
 }
 
-export function checkOperatorValue (value: Value): asserts value is OperatorValue {
+export function checkOperatorValue (value: Value | undefined): asserts value is OperatorValue {
   check(value, ValueType.operator)
 }
 
-export function checkArrayValue (value: Value): asserts value is ArrayValue {
+export function checkArrayValue (value: Value | undefined): asserts value is ArrayValue {
   check(value, ValueType.array)
 }
 
-export function checkDictionaryValue (value: Value): asserts value is DictionaryValue {
+export function checkDictionaryValue (value: Value | undefined): asserts value is DictionaryValue {
   check(value, ValueType.dictionary)
 }
