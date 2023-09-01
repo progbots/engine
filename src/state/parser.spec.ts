f34905fceb7c9d717924c04f101e9eeffd857d5d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-01 16:01:14 -0400

    refactor(state/parser): adjusts tests

--- src/state/parser.spec.ts
+++ src/state/parser.spec.ts
@@ -1,117 +1,137 @@
-import { parse, ParsedValue } from './parser'
-import { Value, ValueType } from '../index'
-import { checkCallValue } from './types'
+import { ValueType, checkCallValue } from '@api'
+import { InternalValue, scanGenericValue } from '@sdk'
+import { parse } from './parser'
+import { getKeys } from '@typescript'
 
-function * parseAll (source: string): Generator<ParsedValue> {
+const FILENAME = 'parser.spec'
+
+function * parseAll (source: string): Generator<InternalValue> {
   let pos = 0
   while (true) {
-    const result = parse(source, pos)
+    const result = parse(source, FILENAME, pos)
     if (result === undefined) {
       break
     }
     yield result
-    pos = result.nextPos
+    pos += result.debug!.length
   }
 }
 
 describe('state/parser', () => {
-  const values: Record<string, Value> = {
-    1: {
+  const values = {
+    '1': {
       type: ValueType.integer,
-      data: 1
+      number: 1
     },
-    123: {
+    '123': {
       type: ValueType.integer,
-      data: 123
+      number: 123
     },
     '+1': {
       type: ValueType.integer,
-      data: 1
+      number: 1
     },
     '+123': {
       type: ValueType.integer,
-      data: 123
+      number: 123
     },
     '-1': {
       type: ValueType.integer,
-      data: -1
+      number: -1
     },
     '-123': {
       type: ValueType.integer,
-      data: -123
+      number: -123
     },
     add: {
       type: ValueType.call,
-      data: 'add'
+      call: 'add'
     },
     '"a"': {
       type: ValueType.string,
-      data: 'a'
+      string: 'a'
     },
     '"abc"': {
       type: ValueType.string,
-      data: 'abc'
+      string: 'abc'
     },
     '[': {
       type: ValueType.call,
-      data: '['
+      call: '['
     },
     ']': {
       type: ValueType.call,
-      data: ']'
+      call: ']'
     },
     '{': {
       type: ValueType.call,
-      data: '{'
+      call: '{'
     },
     '}': {
       type: ValueType.call,
-      data: '}'
+      call: '}'
     },
     '\'123': {
       type: ValueType.call,
-      data: '\'123'
+      call: '\'123'
     },
     ',': {
       type: ValueType.call,
-      data: ','
+      call: ','
     }
   }
 
-  Object.keys(values).forEach(src => {
+  getKeys(values).forEach(src => {
     const expected = values[src]
-    it(`generates ${expected.type} ${JSON.stringify(expected.data)}`, () => {
+    it(`generates ${JSON.stringify(expected)}`, () => {
       const values = [...parseAll(src)]
       expect(values.length).toStrictEqual(1)
       const [value] = values
-      expect(value.source).toStrictEqual(src)
-      expect(value.sourcePos).toStrictEqual(0)
-      const { type, data } = value
-      expect({ type, data }).toStrictEqual(expected)
+      scanGenericValue(value)
+      const { debug } = value
+      if (debug === undefined) {
+        throw new Error('Missing debug information')
+      }
+      expect(debug.source).toStrictEqual(src)
+      expect(debug.pos).toStrictEqual(0)
+      const { type } = value
+      if (![ValueType.integer, ValueType.string, ValueType.call].includes(type)) {
+        throw new Error(`Unexpected value type '${type}`)
+      }
+      expect(value).toMatchObject(expected)
     })
   })
 
   it('generates values', () => {
     const source = '1 2 add'
     const block = [...parseAll(source)]
-    expect(block).toStrictEqual<ParsedValue[]>([{
+    expect(block).toStrictEqual<InternalValue[]>([{
       type: ValueType.integer,
-      data: 1,
-      source,
-      sourcePos: 0,
-      nextPos: 1
+      number: 1,
+      debug: {
+        source,
+        filename: FILENAME,
+        pos: 0,
+        length: 1
+      }
     }, {
       type: ValueType.integer,
-      data: 2,
-      source,
-      sourcePos: 2,
-      nextPos: 3
+      number: 2,
+      debug: {
+        source,
+        filename: FILENAME,
+        pos: 2,
+        length: 1
+      }
     }, {
       type: ValueType.call,
-      data: 'add',
-      source,
-      sourcePos: 4,
-      nextPos: 7
+      call: 'add',
+      debug: {
+        source,
+        filename: FILENAME,
+        pos: 4,
+        length: 3
+      }
     }])
   })
 
@@ -119,7 +139,7 @@ describe('state/parser', () => {
     it('separates array and block symbols', () => {
       const block = [...parseAll('[][[]]]{{}{}}')].map(value => {
         checkCallValue(value)
-        return value.data
+        return value.call
       })
       expect(block).toStrictEqual([
         '[', ']', '[', '[', ']', ']', ']', '{', '{', '}', '{', '}', '}'
@@ -129,7 +149,7 @@ describe('state/parser', () => {
     it('separates array symbols from the names', () => {
       const block = [...parseAll('[abc] abc[')].map(value => {
         checkCallValue(value)
-        return value.data
+        return value.call
       })
       expect(block).toStrictEqual([
         '[', 'abc', ']', 'abc', '['
@@ -139,7 +159,7 @@ describe('state/parser', () => {
     it('separates block symbols from the names', () => {
       const block = [...parseAll('{abc} abc{')].map(value => {
         checkCallValue(value)
-        return value.data
+        return value.call
       })
       expect(block).toStrictEqual([
         '{', 'abc', '}', 'abc', '{'
@@ -155,24 +175,33 @@ describe('state/parser', () => {
 
 add`
     const block = [...parseAll(source)]
-    expect(block).toStrictEqual<ParsedValue[]>([{
+    expect(block).toStrictEqual<InternalValue[]>([{
       type: ValueType.integer,
-      data: 1,
-      source,
-      sourcePos: 21,
-      nextPos: 22
+      number: 1,
+      debug: {
+        source,
+        filename: FILENAME,
+        pos: 21,
+        length: 1
+      }
     }, {
       type: ValueType.integer,
-      data: 2,
-      source,
-      sourcePos: 24,
-      nextPos: 25
+      number: 2,
+      debug: {
+        source,
+        filename: FILENAME,
+        pos: 24,
+        length: 1
+      }
     }, {
       type: ValueType.call,
-      data: 'add',
-      source,
-      sourcePos: 27,
-      nextPos: 30
+      call: 'add',
+      debug: {
+        source,
+        filename: FILENAME,
+        pos: 27,
+        length: 3
+      }
     }])
   })
 })
