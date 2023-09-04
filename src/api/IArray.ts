e936d722385a381773f6aeee5025e40931cbe71b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-03 23:30:52 -0400

    feat(api): helper to enumerate IArray

--- src/api/IArray.ts
+++ src/api/IArray.ts
@@ -4,3 +4,13 @@ export interface IArray {
   readonly length: number
   at: (index: number) => Value | null
 }
+
+export function * getIArrayValues (iArray: IArray): Generator<Value> {
+  const { length } = iArray
+  for (let index = 0; index < length; ++index) {
+    const value = iArray.at(index)
+    if (value !== null) {
+      yield value
+    }
+  }
+}
