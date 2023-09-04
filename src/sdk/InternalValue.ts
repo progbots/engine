d6c43eb1e91b06e1707056e3d30f3f7dc77f5841
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-03 23:31:57 -0400

    feat(sdk): helper to extract debug infos and recognize InternalValue

--- src/sdk/InternalValue.ts
+++ src/sdk/InternalValue.ts
@@ -7,3 +7,12 @@ export type Internal<T> = T & {
 }
 
 export type InternalValue = Internal<Value>
+
+export function checkInternalValue (value: Value): asserts value is InternalValue {
+  // Since additional members are optionals, nothing to check
+}
+
+export function getDebugInfos (value: Value | InternalValue): DebugInfos | undefined {
+  checkInternalValue(value)
+  return value.debug
+}
