1d80537b7ebdcf34f700ece6e3d2097134f8a05c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-05 06:46:56 -0400

    refactor(operators): use aliases

--- src/operators/attributes.ts
+++ src/operators/attributes.ts
@@ -1,10 +1,10 @@
-import { OperatorAttributes, OperatorFunction } from '../state/types'
+import { IOperatorAttributes, IOperatorFunction } from '@sdk'
 import { getKeys } from '../ts-helpers'
 
-export function setOperatorAttributes (operator: OperatorFunction, attributes: OperatorAttributes): void {
+export function setOperatorAttributes (operator: IOperatorFunction, attributes: IOperatorAttributes): void {
   Object.defineProperties(operator, getKeys(attributes).reduce((
     properties: PropertyDescriptorMap,
-    attributeName: keyof OperatorAttributes
+    attributeName: keyof IOperatorAttributes
   ): PropertyDescriptorMap => {
     properties[attributeName] = {
       value: attributes[attributeName],
