ad4d69a8e28c127b926628150ce6edfb2665cfe4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-07 15:25:31 -0400

    refactor(operators): moved to sdk

--- src/operators/debug-infos.ts
+++
@@ -1,6 +0,0 @@
-import { InternalValue, DebugInfos } from '../state/index'
-
-export function extractDebugInfos (value: InternalValue): DebugInfos {
-  const { source, sourcePos, sourceFile } = value
-  return { source, sourcePos, sourceFile }
-}
