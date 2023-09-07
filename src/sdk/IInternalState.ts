ec8528c962e1cfdfe2f4b9c0acc2881d8046a55d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-07 06:53:50 -0400

    feat(sdk): adds IMemoryTracker

--- src/sdk/IInternalState.ts
+++ src/sdk/IInternalState.ts
@@ -1,9 +1,11 @@
 import { IState } from '@api'
 import { ICallStack } from './ICallStack'
 import { IDictionaryStack } from './IDictionaryStack'
+import { IMemoryTracker } from './IMemoryTracker'
 import { IOperandStack } from './IOperandStack'
 
 export interface IInternalState extends IState {
+  readonly memoryTracker: IMemoryTracker
   readonly operands: IOperandStack
   readonly dictionaries: IDictionaryStack
   readonly calls: ICallStack
