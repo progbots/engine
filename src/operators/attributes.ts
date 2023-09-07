62ae42c500126054e5c8b8e5de6b8aad428c4967
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-09-07 05:34:22 -0400

    refactor(operators): improve setOperatorAttributes to remove duplication of types

--- src/operators/attributes.ts
+++ src/operators/attributes.ts
@@ -1,20 +1,35 @@
 import { IOperatorAttributes, IOperatorFunction } from '@sdk'
 import { getKeys } from '../ts-helpers'
+import { ValueType } from '@api'
 
 interface IOperatorAttributesAndName extends IOperatorAttributes {
   name?: string
 }
 
-export function setOperatorAttributes<T1 = any, T2 = any, T3 = any> (
+export function setOperatorAttributes<T1 extends ValueType, T2 extends ValueType, T3 extends ValueType> (
   operator: IOperatorFunction<T1, T2, T3>,
-  attributes: IOperatorAttributesAndName
+  attributes: IOperatorAttributesAndName,
+  typeCheck1?: T1,
+  typeCheck2?: T2,
+  typeCheck3?: T3
 ): void {
-  Object.defineProperties(operator, getKeys(attributes).reduce((
+  const typeCheck: ValueType[] = []
+  if (typeCheck1 !== undefined) {
+    typeCheck.push(typeCheck1)
+  }
+  if (typeCheck2 !== undefined) {
+    typeCheck.push(typeCheck2)
+  }
+  if (typeCheck3 !== undefined) {
+    typeCheck.push(typeCheck3)
+  }
+  const extendedAttributes = Object.assign({ typeCheck }, attributes)
+  Object.defineProperties(operator, getKeys(extendedAttributes).reduce((
     properties: PropertyDescriptorMap,
     attributeName: keyof IOperatorAttributesAndName
   ): PropertyDescriptorMap => {
     properties[attributeName] = {
-      value: attributes[attributeName],
+      value: extendedAttributes[attributeName],
       writable: false
     }
     return properties
