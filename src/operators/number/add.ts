4f102c7345fdd28d69c34d0b1a19514490f1e1f4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2023-10-02 07:28:20 -0400

    refactor(operators): adapts to new format

--- src/operators/number/add.ts
+++ src/operators/number/add.ts
@@ -1,7 +1,7 @@
-import { CycleResult, State } from '../../state/index'
+import { CycleResult, IInternalState } from '@sdk'
 import { compute } from './integer'
 
-export function add (state: State): CycleResult {
+export function add (state: IInternalState): CycleResult {
   compute(state, (value1: number, value2: number) => value1 + value2)
   return null
 }
